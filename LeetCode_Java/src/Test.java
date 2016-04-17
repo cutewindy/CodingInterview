f91ccdea1cb247f6a12395ba3e3e96ca7fa82070
cutewindy <wendi@WindydeMacBook-Air.local>
2016-04-16 21:37:50 -0700

    update

---
+++ LeetCode_Java/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = " the  sky   is blue   ";
+		String[] array = str.split(" ");
+//		for (int i = 0; i < array.length; i ++) {
+//			System.out.println(array[i]);
+//		}
+		System.out.println(array);
+		System.out.println(123);
+//		System.out.println(str.charAt(str.length()-1));
+	}
+
+}
