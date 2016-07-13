8b3545e304711bf567df996271ccaa26a88400c8
cutewindy <wendi@Wendi-MacBook-Air.local>
2016-07-12 22:42:13 -0700

    init

---
+++ LeetCode_Java/src/SearchinRotatedSortedArrayII.java
@@ -0,0 +1,63 @@
+/**
+ * Follow up for "Search in Rotated Sorted Array":
+ * What if duplicates are allowed? 
+ * Would this affect the run-time complexity? How and why?
+ * Write a function to determine if a given target is in the array.
+ * 
+ * Tags: 
+ * @author wendi
+ *
+ */
+public class SearchinRotatedSortedArrayII {
+
+	/**
+	 * BinarySearch: (Template) need to deal with duplicated number. The same like "findMinimuminRotatedSortedArrayII"
+	 * @param int[] nums, int target
+	 * @return boolean
+	 * Time: O(n) worst case
+	 * Space: O(1)
+	 */
+	public boolean searchinRotatedSortedArrayII(int[] nums, int target) {
+		if (nums == null || nums.length == 0) {
+			return false;
+		}
+		int start = 0; 
+		int end = nums.length - 1;
+		while (start + 1 < end) {
+			int mid = start + (end - start) / 2;
+			if (nums[mid] == target) {
+				return true;
+			}
+			else if (nums[mid] == nums[end]) {    // It's fine to remove nums[end]
+				end--;
+			}
+			else if (nums[mid] < nums[end]) {
+				if (nums[mid] < target && target <= nums[end]) {
+					start = mid + 1;
+				}
+				else {
+					end = mid - 1;
+				}
+			}
+			else {
+				if (nums[start] <= target && target < nums[mid]) {
+					end = mid - 1;
+				}
+				else {
+					start = mid + 1;
+				}
+			}
+		}
+		if (nums[start] == target || nums[end] == target) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SearchinRotatedSortedArrayII result = new SearchinRotatedSortedArrayII();
+		System.out.println(result.searchinRotatedSortedArrayII(new int[] {1, 1, 1, 1, 0, 1, 1},  0));
+	}
+
+}
