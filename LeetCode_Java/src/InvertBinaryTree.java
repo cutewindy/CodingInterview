4270b79418ca1f9a76462f2a3c7b45ed1ba077e2
cutewindy <wendi@Wendi-MacBook-Air.local>
2016-07-22 11:47:28 -0700

    update

--- LeetCode_Java/src/InvertBinaryTree.java
+++ LeetCode_Java/src/InvertBinaryTree.java
@@ -1,45 +1,97 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * Invert a binary tree.
-
-	     4
-	   /   \
-	  2     7
-	 / \   / \
-	1   3 6   9
-	to
-	     4
-	   /   \
-	  7     2
-	 / \   / \
-	9   6 3   1
+		     4
+		   /   \
+		  2     7
+		 / \   / \
+		1   3 6   9
+ * to
+		     4
+		   /   \
+		  7     2
+		 / \   / \
+		9   6 3   1
+ *
+ * Tags: Tree
  * @author wendi
  *
  */
 public class InvertBinaryTree {
+	
 	/**
-	 * Method1(Iteration): using queue to record the next time which node swap its left and right,
-	 * before swap, add left and right to q.
+	 * Method4:(DFS: Iteration). Pop node from stack, and swap it's left and right.Then push it's right and left into stack.
+	 * (Do the swapping inorder)
 	 * @param TreeNode root
-	 * @return TreeNode root
+	 * @return TreeNode
 	 * Time: O(n)
 	 * Space: O(n)
 	 */
-	public TreeNode invertBinaryTree(TreeNode root) {
+	public TreeNode invertBinaryTreeIII(TreeNode root) {
 		if (root == null) {
 			return root;
 		}
-		Queue<TreeNode> q = new LinkedList();
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(root);
+		while (!stack.isEmpty()) {
+			TreeNode curr = stack.pop();
+			swap(curr, curr.left, curr.right);
+			if (curr.right != null) {
+				stack.push(curr.right);
+			}
+			if (curr.left != null) {
+				stack.push(curr.left);
+			}
+		}
+		return root;
+	}
+	
+	/**
+	 * Method3:(DFS: Recursion) swap curr node's left and right. Then going down let left and right do the same thing.
+	 * @param TreeNode root
+	 * @return TreeNode
+	 * Time: O(n)
+	 * Space: O(1)
+	 * Stack space: O(log(n))
+	 */
+	public TreeNode invertBinaryTreeII(TreeNode root) {
+		if (root == null) {
+			return root;
+		}
+		helper(root);
+		return root;
+	}
+	private void helper(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		swap(root, root.left, root.right);
+		helper(root.left);
+		helper(root.right);
+	}
+	
+	/**
+	 * Method2(BFS): using queue to record the next time which node swap its left and right,
+	 * before swap, add left and right to q.
+	 * @param TreeNode root
+	 * @return TreeNode
+	 * Time: O(n)
+	 * Space: O(n)
+	 */
+	public TreeNode invertBinaryTreeI(TreeNode root) {
+		if (root == null) {
+			return root;
+		}
+		Queue<TreeNode> q = new LinkedList<>();
 		q.add(root);
 		while (!q.isEmpty()) {
 			TreeNode node = q.poll();
 			if (node != null) {
-				q.add(node.left);
-				q.add(node.right);
+				q.offer(node.left);
+				q.offer(node.right);
 				swap(node, node.left, node.right);
 			}
 		}
@@ -47,26 +99,29 @@ public class InvertBinaryTree {
 	}
 	
 	/**
-	 * Method1(Recursion): if root is not null, swap its left and right, 
-	 * and let left and right separately as root go into recursion.
+	 * Method1(Backtracking): if root is not null, let left and right separately as root go into recursion do swapping.
+	 * and then swap left and right, 
 	 * @param TreeNode root
-	 * @return TreeNode root
+	 * @return TreeNode
 	 * Time: O(n)
 	 * Space: O(1)
 	 * Stack space: O(log(n))
 	 */
-	public TreeNode invertBinaryTreeI(TreeNode root) {
+	public TreeNode invertBinaryTree(TreeNode root) {
 		if (root == null) {
 			return root;
 		}
-		swap(root, root.left, root.right);
-		invertBinaryTree(root.left);
-		invertBinaryTree(root.right);
+		TreeNode left = invertBinaryTree(root.left);
+		TreeNode right = invertBinaryTree(root.right);
+		swap(root, left, right);
 		return root;
 	}
 	
-	
-	private static void swap(TreeNode node, TreeNode left, TreeNode right) {
+	/**
+	 * Do swapping
+	 * @param TreeNode node, TreeNode left, TreeNode right
+	 */
+	private void swap(TreeNode node, TreeNode left, TreeNode right) {
 		TreeNode temp = left;
 		node.left = right;
 		node.right = temp;
@@ -79,11 +134,10 @@ public class InvertBinaryTree {
 		TreeNode root = TreeNode.generateCBT(array);
 		TreeNode.printCBT(root);
 		System.out.println("-----------------");
-//		TreeNode newRoot = result.invertBinaryTree(root);
-//		TreeNode.printCompleteBinaryTree(newRoot);
-		
-		TreeNode newRoot1 = result.invertBinaryTreeI(root);
-		TreeNode.printCBT(newRoot1);
+//		TreeNode.printCBT(result.invertBinaryTree(root));
+//		TreeNode.printCBT(result.invertBinaryTreeI(root));
+//		TreeNode.printCBT(result.invertBinaryTreeII(root));
+		TreeNode.printCBT(result.invertBinaryTreeIII(root));
 	}
 
 }
