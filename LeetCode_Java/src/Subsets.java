01b007ada345536bea1f8d5be3e5b7e6f92cd5aa
cutewindy <wendi@Wendi-MacBook-Air.local>
2016-07-30 15:19:35 -0700

    init

---
+++ LeetCode_Java/src/Subsets.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a set of distinct integers, nums, return all possible subsets.
+ * Note: The solution set must not contain duplicate subsets.
+ * For example,
+ * If nums = [1,2,3], a solution is:
+ * [
+ *   [3],
+ *   [1],
+ *   [2],
+ *   [1,2,3],
+ *   [1,3],
+ *   [2,3],
+ *   [1,2],
+ *   []
+ * ]
+ * 
+ * Tags: Array, Backtracking, Bit Manipulation
+ * @author wendi
+ *
+ */
+public class Subsets {
+
+	/**
+	 * Method3: Bit Manipulation
+	 * @param int[] nums
+	 * @return List<List<Integer>>
+	 * Time: O(2^n)
+	 * Space: O(n)
+	 */
+	public  List<List<Integer>> subsetsII(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (nums == null || nums.length == 0) {
+			return result;
+		}
+		return result;
+	}
+	
+	
+	/**
+	 * Method2: BFS: Iteration:This problem can also be solved iteratively. 
+	 * Take [1, 2, 3] in the problem statement as an example. The process of generating all the 
+	 * subsets is like:
+	 * 1. Initially: [[]]
+	 * 2. Adding the first number to all the existed subsets: [[], [1]];
+	 * 3. Adding the second number to all the existed subsets: [[], [1], [2], [1, 2]];
+	 * 4. Adding the third number to all the existed subsets: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]].
+	 * @param int[] nums
+	 * @return List<List<Integer>>
+	 * Time: O(2^n)
+	 * Space: O(n)
+	 */
+	public  List<List<Integer>> subsetsI(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (nums == null || nums.length == 0) {
+			return result;
+		}
+		List<Integer> combo = new ArrayList<>();
+		result.add(combo);
+		for (int i = 0; i < nums.length; i++) {
+			int size = result.size();
+			for (int j = 0; j < size; j++) {
+				combo = new ArrayList(result.get(j));
+				combo.add(nums[i]);
+				result.add(combo);
+			}
+		}
+		return result;
+	}
+	
+	
+	/**
+	 * Method1: DFS/ Backtracking
+	 * @param int[] nums
+	 * @return List<List<Integer>>
+	 * Time: O(2^n)
+	 * Space: O(n)
+	 * Stack space: O(n)
+	 */
+	public List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (nums == null || nums.length == 0) {
+			return result;
+		}
+		List<Integer> combo = new ArrayList<>();
+		helper(nums, 0, combo, result);
+		return result;
+	}
+	
+	private void helper(int[] nums, int pos, List<Integer> combo, List<List<Integer>> result) {
+		result.add(new ArrayList<>(combo));
+		if (pos == nums.length) {
+			return;
+		}
+		for (int i = pos; i < nums.length; i++) {
+			combo.add(nums[i]);
+			helper(nums, i + 1, combo, result);
+			combo.remove(combo.size() - 1);
+		}
+		return;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Subsets result = new Subsets();
+		System.out.println(result.subsets(new int[] {1, 2, 3}));
+		System.out.println(result.subsetsI(new int[] {1, 2, 3}));
+		System.out.println(result.subsetsII(new int[] {1, 2, 3}));
+	}
+
+}
---
+++ LeetCode_Java/src/SubsetsII.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+ * Note: The solution set must not contain duplicate subsets.
+ * For example,
+ * If nums = [1,2,2], a solution is:
+ * [
+ *  [2],
+ *  [1],
+ *  [1,2,2],
+ *  [2,2],
+ *  [1,2],
+ *  []
+ * ]
+ * 
+ * Tags: Array, Backtracking
+ * @author wendi
+ *
+ */
+public class SubsetsII {
+
+	/**
+	 * Method2:BFS Iteration: Like subsetI. If nums[i] is duplicate, combo start at the size getting
+	 * from last level. Otherwise, combo start at 0.
+	 * @param nums
+	 * @return List<List<Integer>>
+	 * Time: O(2^n)
+	 * Space: O(n)
+	 */
+	public List<List<Integer>> subsetsIII(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (nums == null || nums.length == 0) {
+			return result;
+		}
+		Arrays.sort(nums);
+		List<Integer> empty = new ArrayList<>();
+		result.add(empty);
+		int start = 0;
+		for (int i = 0; i < nums.length; i++) {
+			int size = result.size();
+			if (i == 0 || nums[i] != nums[i - 1]) { // nums[i] is not duplicate, combine from beginning
+				start = 0;
+			}
+			for (int j = start; j < size; j++) {
+				List<Integer> combo = new ArrayList<>(result.get(j));
+				combo.add(nums[i]);
+				result.add(combo);
+			}
+			start = size;
+		}
+		return result;
+	}
+	
+	/**
+	 * Method1: DFS(recursion)
+	 * @param int[] nums
+	 * @return List<List<Integer>>
+	 * Time: O(2^n)
+	 * Space: O(n)
+	 * Stack space: O(n)
+	 */
+	public List<List<Integer>> subsetsII(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (nums == null || nums.length == 0) {
+			return result;
+		}
+		Arrays.sort(nums);
+		List<Integer> combo = new ArrayList<>();
+		helper(nums, 0, combo, result);
+		return result;
+	}
+	
+	private void helper(int[] nums, int pos, List<Integer> combo, List<List<Integer>> result) { 
+		result.add(new ArrayList<>(combo));
+		for (int i = pos; i < nums.length; i++) {
+			if (i != pos && nums[i] == nums[i - 1]) {  // duplicate
+				continue;
+			}
+			combo.add(nums[i]);
+			helper(nums, i + 1, combo, result);
+			combo.remove(combo.size() - 1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SubsetsII result = new SubsetsII();
+//		System.out.println(result.subsetsII(new int[] {1, 2, 2}));
+//		System.out.println(result.subsetsII(new int[] {4, 4, 4, 1, 4}));
+		System.out.println(result.subsetsIII(new int[] {4, 4, 4, 1, 4}));
+	}
+
+}
