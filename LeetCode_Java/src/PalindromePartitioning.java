949e6c81e6190bb522d4a4e022d853c9151040ae
cutewindy <wendi@Wendi-MacBook-Air.local>
2016-08-02 00:43:04 -0700

    init

---
+++ LeetCode_Java/src/PalindromePartitioning.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a string s, partition s such that every substring of the partition is a palindrome.
+ * Return all possible palindrome partitioning of s.
+ * For example, given s = "aab",
+ * Return
+ * [
+ *   ["aa","b"],
+ *   ["a","a","b"]
+ * ]
+ * 
+ * Tags: Backtracking
+ * @author wendi
+ *
+ */
+public class PalindromePartitioning {
+
+	/**
+	 * Backtracking:
+	 * @param String s
+	 * @return List<List<Integer>>
+	 * Time:O(n * n!)
+	 * Space: O(n) n = s.length()
+	 * Stack space: O(n)
+	 */
+	public List<List<String>> palindromePartitioning(String s) {
+		List<List<String>> result = new ArrayList<>();
+		if (s == null || s.length() == 0) {
+			return result;
+		}
+		helper(s, 0, new ArrayList<String>(), result);
+		return result;
+	}
+	
+	private void helper(String s, int pos, List<String> combo, List<List<String>> result) {
+		if (pos == s.length()) {
+			result.add(new ArrayList<>(combo));
+			return;
+		}
+		for (int i = pos; i < s.length(); i++) {
+			if (isPalindrome(s, pos, i)) {
+				combo.add(s.substring(pos, i + 1));
+				helper(s, i + 1, combo, result);
+				combo.remove(combo.size() - 1);
+			}
+		}
+	}
+	
+	private boolean isPalindrome(String s, int start, int end) {
+		while (start < end) {
+			if (s.charAt(start++) != s.charAt(end--)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PalindromePartitioning result = new PalindromePartitioning();
+		System.out.println(result.palindromePartitioning("aab"));
+	}
+
+}
---
+++ LeetCode_Java/src/SurroundedRegions.java
@@ -0,0 +1,93 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+ * A region is captured by flipping all 'O's into 'X's in that surrounded region.
+ * For example,
+		X X X X
+		X O O X
+		X X O X
+		X O X X
+ * After running your function, the board should be:
+		X X X X
+		X X X X
+		X X X X
+		X O X X
+ * 
+ * Tags: BFS, Union find
+ * @author wendi
+ *
+ */
+public class SurroundedRegions {
+
+	/**
+	 * BFS: 
+	 * @param char[][] board
+	 * Time: O(n^2)
+	 * Space: O(1)
+	 */
+	public void surroundedRegions(char[][] board) {
+		if (board == null || board.length == 0 || board[0].length == 0) {
+			return;
+		}
+		int m = board.length;
+		int n = board[0].length;
+		Queue<int[]> queue = new LinkedList<>();
+		// do the init
+		for (int col = 0; col < n; col++) {
+			if (board[0][col] == 'O') {               // 1 top line
+				queue.offer(new int[] {0, col});
+			}
+			if (board[m - 1][col] == 'O') {
+				queue.offer(new int[] {m - 1, col});  // 2 down line
+			}
+		}
+		for (int row = 0; row < m; row++) {
+			if (board[row][0] == 'O') {
+				queue.offer(new int[] {row, 0});      // 3 left line
+			}
+			if (board[row][n - 1] == 'O') {
+				queue.offer(new int[] {row, n - 1});  // 4 right line
+			}
+		}
+		// BFS to find the union 'O' of edge, if it's union of edge, set 'Y'
+		while (!queue.isEmpty()) {
+			int[] point = queue.poll();
+			int row = point[0];
+			int col = point[1];
+			if (board[row][col] == 'Y') continue; // already change to 'X'
+			board[row][col] = 'Y';
+			if (row > 0 && board[row - 1][col] == 'O') queue.offer(new int[] {row - 1, col});      // check top
+			if (row < m - 1 && board[row + 1][ col] == 'O') queue.offer(new int[] {row + 1, col}); // check down
+			if (col > 0 && board[row][col - 1] == 'O') queue.offer(new int[] {row, col - 1});      // check left
+			if (col < n - 1 && board[row][col + 1] == 'O') queue.offer(new int[] {row, col + 1});  // check right
+		}
+		// convert 'Y' to 'O', 'O' to 'X'
+		for (int row = 0; row < m; row++) {
+			for (int col = 0; col < n; col++) {
+				if (board[row][col] == 'O') {
+					board[row][col] = 'X';
+				}
+				if (board[row][col] == 'Y') {
+					board[row][col] = 'O';
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SurroundedRegions result = new SurroundedRegions();
+		char[][] board = {{'X', 'X', 'X', 'X'}, 
+						 {'X', 'O', 'O', 'X'}, 
+						 {'X', 'X', 'O', 'X'}, 
+						 {'X', 'O', 'X', 'X'}};
+		result.surroundedRegions(board);
+		for (char[] bo: board) {
+			System.out.println(Arrays.toString(bo));
+		}
+	}
+
+}
