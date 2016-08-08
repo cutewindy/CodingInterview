d25acfeb463d6faf14d976b249e08dab3753e2ee
cutewindy <wendi@Wendi-MacBook-Air.local>
2016-08-08 12:54:04 -0700

    init

--- LeetCode_Java/src/ValidAnagram.java
+++ LeetCode_Java/src/ValidAnagram.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Given two strings s and t, write a function to determine if t is an anagram of s.
@@ -9,37 +10,68 @@ import java.util.HashMap;
  * You may assume the string contains only lowercase alphabets.
  * Follow up:
  * What if the inputs contain unicode characters? How would you adapt your solution to such case?
+ * 
+ * Tags: HashTable, Sort
  * @author wendi
  *
  */
 public class ValidAnagram {
+	
 	/**
-	 * use HashMap to record the frequency of each char in s,
-	 * check whether hash contains the char of t, and the hash value of that char > 0
-	 * @param String s
-	 * @param String t
+	 * Method2: Using array instead of hash to save the frequency of each char
+	 * @param String s, String t
 	 * @return boolean
 	 * Time: O(n)
-	 * Space: O(n)
+	 * Space: O(1)
 	 */
-	public boolean validAnagram(String s, String t) {
-		if (s == null || t == null || s.length() != t.length()) {
+	public boolean validAnagramI(String s, String t) {
+		if (s.length() != t.length()) {
 			return false;
 		}
-		HashMap<Character, Integer> hash = new HashMap<Character, Integer>();
-		for (int i = 0; i < s.length(); i++) {
-			if (hash.containsKey(s.charAt(i))) {
-				hash.put(s.charAt(i), hash.get(s.charAt(i)) + 1);
-			}
-			else {
-				hash.put(s.charAt(i), 1);
-			}
+		if (s == null || t == null) {
+			return true;
 		}
-		for (int i = 0; i < t.length(); i++) {
-			if (!hash.containsKey(t.charAt(i)) || hash.get(t.charAt(i)) == 0) {
+		int[] array = new int[26];
+		for (char c: s.toCharArray()) {
+			array[c - 'a']++;
+		}
+		for (char c: t.toCharArray()) {
+			if (--array[c - 'a'] < 0) {
 				return false;
 			}
-			hash.put(t.charAt(i), hash.get(t.charAt(i)) + 1);
+		}
+		return true;
+	}
+	
+	/**
+	 * Method1: use HashMap to record the frequency of each char in s,
+	 * check whether hash contains the char of t, and the hash value of that char > 0
+	 * @param String s, String t
+	 * @return boolean
+	 * Time: O(n)
+	 * Space: O(1)
+	 */
+	public boolean validAnagram(String s, String t) {
+		if (s.length() != t.length()) {
+			return false;
+		}
+		if (s == null || t == null) {
+			return true;
+		}
+		Map<Character, Integer> hash = new HashMap<>();
+		for (char c: s.toCharArray()) {
+			if (hash.containsKey(c)) {
+				hash.put(c, hash.get(c) + 1);
+			}
+			else {
+				hash.put(c, 1);
+			}
+		}
+		for (char c: t.toCharArray()) {
+			if (!hash.containsKey(c) || hash.get(c) - 1 < 0) {
+				return false;
+			}
+			hash.put(c, hash.get(c) - 1);
 		}
 		return true;
 	}
@@ -48,7 +80,7 @@ public class ValidAnagram {
 		// TODO Auto-generated method stub
 		ValidAnagram result = new ValidAnagram();
 		System.out.println(result.validAnagram("taa", "ate"));
-
+		System.out.println(result.validAnagramI("taa", "ate"));
 	}
 
 }
