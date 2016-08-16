729d6f3430272100ac6e6d7bb83183dc006a5fd6
cutewindy <wendi@Wendi-MacBook-Air.local>
2016-08-15 21:58:25 -0700

    init

--- LeetCode_Java/src/MergekSortedLists.java
+++ LeetCode_Java/src/MergekSortedLists.java
@@ -1,3 +1,7 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Comparator;
+
 /**
  * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
  * 
@@ -8,25 +12,47 @@
 public class MergekSortedLists {
 
 	/**
-	 * Method3: Heap
+	 * Method3: Minimum Heap
 	 * @param ListNode lists
 	 * @return ListNode
-	 * Time: O()
-	 * Space: O()
+	 * Time: O(nklog(k))
+	 * Space: O(k)
 	 */
 	public ListNode mergekSortedListsII(ListNode[] lists) {
 		if (lists == null || lists.length == 0) {
 			return null;
 		}
 		ListNode dummy = new ListNode(0);
+		ListNode curr = dummy;
+		Queue<ListNode> minHeap = new PriorityQueue<>(10, new Comparator<ListNode>() {
+			@Override
+			public int compare(ListNode n1, ListNode n2) {
+				return n1.val - n2.val;  // min heap
+			}
+		});
+		// 1 add head of each list
+		for (ListNode head: lists) {
+			if (head != null) {  // be care about head != null
+				minHeap.offer(head);
+			}
+		}
+		// 2 poll the min node from heap, and add the next node of min into heap if next!=null
+		while (!minHeap.isEmpty()) {
+			curr.next = minHeap.poll();
+			if (curr.next.next != null) {
+				minHeap.offer(curr.next.next);
+			}
+			curr = curr.next;
+		}
 		return dummy.next;
 	}	
+	
 	
 	/**
 	 * Method2: MergeSort + Divide and Conquer
 	 * @param ListNode lists
 	 * @return ListNode
-	 * Time: O(knlong(n))
+	 * Time: O(knlog(k))
 	 * Space: O(1)
 	 */
 	public ListNode mergekSortedListsI(ListNode[] lists) {
@@ -87,9 +113,9 @@ public class MergekSortedLists {
 		ListNode l1 = ListNode.generateLinkedList(new int[] {1, 3, 6, 7});
 		ListNode l2 = ListNode.generateLinkedList(new int[] {2, 5});
 		ListNode l3 = ListNode.generateLinkedList(new int[] {4, 8, 9});
-		ListNode.printLinkedList(result.mergekSortedLists(new ListNode[] {l1, l2, l3}));
-		ListNode.printLinkedList(result.mergekSortedListsI(new ListNode[] {l1, l2, l3}));
-//		ListNode.printLinkedList(result.mergekSortedListsII(new ListNode[] {l1, l2, l3}));
+//		ListNode.printLinkedList(result.mergekSortedLists(new ListNode[] {l1, l2, l3}));
+//		ListNode.printLinkedList(result.mergekSortedListsI(new ListNode[] {l1, l2, l3}));
+		ListNode.printLinkedList(result.mergekSortedListsII(new ListNode[] {l1, l2, l3}));
 	}
 
 }
