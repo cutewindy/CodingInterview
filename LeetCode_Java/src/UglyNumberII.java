4c7240d8d09ddf6e2de7d587e588640e41a5e86e
cutewindy <wendi@Wendi-MacBook-Air.local>
2016-08-29 17:16:29 -0700

    init

--- LeetCode_Java/src/UglyNumberII.java
+++ LeetCode_Java/src/UglyNumberII.java
@@ -3,12 +3,15 @@ import java.util.List;
 
 /**
  * Write a program to find the n-th ugly number.
- * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 
+ * 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
  * Note that 1 is typically treated as an ugly number.
  * Hint:
- * The naive approach is to call isUgly for every number until you reach the nth one. Most numbers are not ugly. Try to focus your effort on generating only the ugly ones.
+ * The naive approach is to call isUgly for every number until you reach the nth one. Most numbers 
+ * are not ugly. Try to focus your effort on generating only the ugly ones.
  * An ugly number must be multiplied by either 2, 3, or 5 from a smaller ugly number.
- * The key is how to maintain the order of the ugly numbers. Try a similar approach of merging from three sorted lists: L1, L2, and L3.
+ * The key is how to maintain the order of the ugly numbers. Try a similar approach of merging from 
+ * three sorted lists: L1, L2, and L3.
  * Assume you have Uk, the kth ugly number. Then Uk+1 must be Min(L1 * 2, L2 * 3, L3 * 5).
  * 
  * Tags: DP, heap, Math
@@ -18,7 +21,8 @@ import java.util.List;
 public class UglyNumberII {
 
 	/**
-	 * DP: use the definition of ugly number, the new ugly number always minimum of the old ugly number *2, *3 and *5.
+	 * DP: use the definition of ugly number, the new ugly number always minimum of the old ugly 
+	 * number *2, *3 and *5.
 	 * Use a list to save the old ugly number, and i2, i3 and i5 as pointers to record the position.
 	 * @param int n
 	 * @return int
@@ -26,15 +30,15 @@ public class UglyNumberII {
 	 * Space: O(n)
 	 */
 	public int unlyNumberII(int n) {
-		if (n == 0) {
+		if (n <= 0) {
 			return 0;
 		}
-		List<Integer> ugly = new ArrayList();
+		List<Integer> ugly = new ArrayList<>();
 		ugly.add(1);
 		int i2 = 0;
 		int i3 = 0;
 		int i5 = 0;
-		for (int i = 1; i < n; i++) {
+		for (int i = 2; i <= n; i++) {
 			while (ugly.get(i2) * 2 <= ugly.get(ugly.size() - 1)) {
 				i2++;
 			}
@@ -53,7 +57,7 @@ public class UglyNumberII {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UglyNumberII result = new UglyNumberII();
-		System.out.println(result.unlyNumberII(10));
+		System.out.println(result.unlyNumberII(11));
 	}
 
 }
