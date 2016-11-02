d74c104bffda2f3f3fcc626edb95e8601bb79fb3
cutewindy <wendi@Wendi-MacBook-Air.local>
2016-11-01 22:35:46 -0700

    init

--- LeetCode_Java/src/SmallestRectangleEnclosingBlackPixels.java
+++ LeetCode_Java/src/SmallestRectangleEnclosingBlackPixels.java
@@ -19,30 +19,78 @@
 public class SmallestRectangleEnclosingBlackPixels {
 	
 	/**
-	 * 
+	 * Binary Search: find the left boundary, do the binary search in the [0, y] range and find the 
+	 * first column vector who has any black pixel.
+	 * To determine if a column vector has a black pixel is O(m) so the search in total is O(mlogn).
+	 * Do the same for the other boundaries. The area is then calculated by the boundaries.
 	 * @param char[][] image, int x, int y
 	 * @return int 
-	 * Time: O()
-	 * Space: O()
+	 * Time: O(2mlog(n) + 2nlog(m))
+	 * Space: O(1)
 	 */
 	public int smallestRectangleEnclosingBlackPixels(char[][] image, int x, int y) {
 		if (image == null || image.length == 0 || image[0].length == 0) {
 			return 0;
 		}
-		int start;
-		int end;
+		int start, end, i, j;
+		int left, right, top, bottom;
 		int m = image.length;
 		int n = image[0].length;
 		// find left bound
 		start = 0;
 		end = y;
 		while (start + 1 < end) {
-			int mid = start + (end - start) / 2;
-			for (int i = 0; i < m; i++) {
-				
+			i = 0;
+			j = start + (end - start) / 2;
+			while (i < m && image[i][j] == '0') {
+				i++;
 			}
+			if (i < m) end = j;
+			else start = j;
 		}
-		return 0;
+		i = 0;
+		while (i < m && image[i][start] == '0') i++;
+		left = i < m ? start : end;			
+		// find right bound
+		start = y;
+		end = n - 1;
+		while (start + 1 < end) {
+			i = 0;
+			j = start + (end - start) / 2;
+			while (i < m && image[i][j] == '0') i++;
+			if (i < m) start = j;
+			else end = j;
+		}
+		i = 0;
+		while (i < m && image[i][end] == '0') i++;
+		right = i < m ? end : start;
+		// find top bound
+		start = 0;
+		end = x;
+		while (start + 1 < end) {
+			j = 0;
+			i = start + (end - start) / 2;
+			while (j < n && image[i][j] == '0') j++;
+			if (j < n) end = i;
+			else start = i;
+		}
+		j = 0;
+		while (j < n && image[start][j] == '0') j++;
+		top = j < n ? start : end;
+		// find bottom bound
+		start = x;
+		end = m - 1;
+		while (start + 1 < end) {
+			j = 0;
+			i = start + (end - start) / 2;
+			while (j < n && image[i][j] == '0') j++;
+			if (j < n) start = i;
+			else end = i;
+		}
+		j = 0;
+		while (j < n && image[end][j] == '0') j++;
+		bottom = j < n ? end : start;
+		return (right - left + 1) * (bottom - top + 1);
 	}
 
 	public static void main(String[] args) {
@@ -50,7 +98,7 @@ public class SmallestRectangleEnclosingBlackPixels {
 		SmallestRectangleEnclosingBlackPixels result = new SmallestRectangleEnclosingBlackPixels();
 		System.out.println(result.smallestRectangleEnclosingBlackPixels(new char[][] 
 				{{'0', '0', '1', '0'},
-				 {'0', '1', '1', '0'},
+				 {'0', '0', '1', '0'},
 				 {'0', '1', '0', '0'},
 				 {'0', '0', '0', '0'}},
 				0, 2));
