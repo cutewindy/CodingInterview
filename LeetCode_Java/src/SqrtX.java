2f1dce831373699db284e1dd4f0322bbcd471782
cutewindy <wendi@Wendi-MacBook-Air.local>
2016-11-06 23:50:01 -0800

    init

---
+++ LeetCode_Java/src/SqrtX.java
@@ -0,0 +1,40 @@
+/**
+ * Implement int sqrt(int x).
+ * Compute and return the square root of x.
+ * 
+ * Tags: Binary Search, Math
+ * @author wendi
+ *
+ */
+public class SqrtX {
+	
+	/**
+	 * Binary Search
+	 * @param int x
+	 * @return int
+	 * Time: O(log(n))
+	 * Space: O(1)
+	 */
+	public int sqrtX(int x) {
+		if (x <= 0) return 0;
+		int start = 1;
+		int end = x;
+		while (start + 1 < end) {
+			int mid = start + (end - start) / 2;
+			if ((long) mid * mid < x) {
+				start = mid;
+			}
+			else {
+				end = mid;
+			}
+		}
+		return (long) end * end <= x ? end : start;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SqrtX result = new SqrtX();
+		System.out.println(result.sqrtX(10));
+	}
+
+}
