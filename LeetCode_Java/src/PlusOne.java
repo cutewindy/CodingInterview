d069354445e52d84b76e83b2154ee2c94df9d5e8
cutewindy <wendi@Wendi-MacBook-Air.local>
2016-12-31 12:30:40 -0800

    update

--- LeetCode_Java/src/PlusOne.java
+++ LeetCode_Java/src/PlusOne.java
@@ -21,14 +21,15 @@ public class PlusOne {
 		if (digits == null || digits.length == 0) {
 			return new int[] {1}; 
 		}
-		for (int i = digits.length - 1; i >= 0; i--) {
+		int n = digits.length;
+		for (int i = n - 1; i >= 0; i--) {
 			if (digits[i] < 9) {
 				digits[i] += 1;
 				return digits;
 			}
 			digits[i] = 0;
 		}
-		int[] result = new int[digits.length + 1];
+		int[] result = new int[n + 1];
 		result[0] = 1;
 		return result;
 	}
