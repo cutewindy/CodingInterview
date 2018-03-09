ba0f99f13e2e24fee7c3a79a254e6fbe4ce0841a
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-03-09 22:25:36 +0800

    update

--- LeetCode_Java/src/BulbSwitcher.java
+++ LeetCode_Java/src/BulbSwitcher.java
@@ -18,12 +18,12 @@
 public class BulbSwitcher {
 
 	/**
-	 * A light will be toggled only during the round of its factors, e.g. number 6 light will be 
-	 * toggled at 1,2,3,6 and light 12 will be toggled at 1,2,3,4,6,12. The final state of a light 
-	 * is on and off only depends on if the number of its factor is odd or even. If odd, the light 
-	 * is on and if even the light is off. The number of one number's factor is odd if and only if 
-	 * it is a perfect square! So we will only need to loop to find all the perfect squares that 
-	 * are smaller than n!
+	 * Math
+	 * Call them bulb 1 to bulb n. Bulb i is switched in round d if and only if d divides i. So bulb 
+	 * i ends up on if and only if it has an odd number of divisors.
+	 * Divisors come in pairs, like i=12 has divisors 1 and 12, 2 and 6, and 3 and 4. Except when i 
+	 * is a square, like 36 has divisors 1 and 36, 2 and 18, 3 and 12, 4 and 9, and double divisor 6. 
+	 * So bulb i ends up on if and only if i is a square.
 	 * @param int n
 	 * @return int
 	 * Time: O(1)
