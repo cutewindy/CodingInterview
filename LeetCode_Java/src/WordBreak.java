c8f358f270c6082dc066a070cf58a8aeb98150b8
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-03-22 00:09:01 +0800

    update

--- LeetCode_Java/src/WordBreak.java
+++ LeetCode_Java/src/WordBreak.java
@@ -18,28 +18,24 @@ public class WordBreak {
 	
 	/**
 	 * DP: dp[i]: whether s[0..i-1] can be segmented with dictionary words.
-	 * dp[i] = dp[j - 1] && wordDict.contains(s[j..i-1]).
+	 * dp[j] = dp[i - 1] && wordDict.contains(s[i..j-1]).
 	 * @param String s, Set<String> wordDict
 	 * @return boolean
 	 * Time: O(n^2)
 	 * Space: O(n)
 	 */
 	public boolean wordBreak(String s, Set<String> wordDict) {
-		if (s == null && wordDict == null) return true;
-		if (s == null || s.length() == 0) return true;
-		if (wordDict == null || wordDict.size() == 0) return false;
-		int n = s.length();
-		boolean[] dp = new boolean[n + 1];
-		dp[0] = true;
-		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j < i; j++) {
-				if (dp[j] && wordDict.contains(s.substring(j, i))) {
-					dp[i] = true;
-					break;
-				}
-			}
-		}
-		return dp[n];
+        if (s == null || s.length() == 0) return true;
+        boolean[] dp = new boolean[s.length() + 1];
+        // init
+        dp[0] = true;
+        // update
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = i; j <= s.length(); j++) {
+                if (wordDict.contains(s.substring(i - 1, j)) && dp[i - 1]) dp[j] = true;
+            }
+        }
+        return dp[s.length()];
 	}
 
 	public static void main(String[] args) {
