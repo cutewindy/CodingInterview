30a7265c9f9c8da13dc41e932801007ed9f6e7f4
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-06-06 16:31:26 -0700

    update

--- LeetCode_Java/src/ReverseBits.java
+++ LeetCode_Java/src/ReverseBits.java
@@ -24,7 +24,8 @@ public class ReverseBits {
 		if (n == 0) {
 			return 0;
 		}
-		n = (n >>> 16) | (n << 16);
+		n = (n >>> 16) | (n << 16); 
+//		n = ((n & 0xffff0000) >>> 16) | ((n & 0x0000ffff) << 16);
 		n = ((n & 0xff00ff00) >>> 8) | ((n & 0x00ff00ff) << 8);
 		n = ((n & 0xf0f0f0f0) >>> 4) | ((n & 0x0f0f0f0f) << 4);
 		n = ((n & 0xcccccccc) >>> 2) | ((n & 0x33333333) << 2);
@@ -40,13 +41,9 @@ public class ReverseBits {
 	 * Space: O(1)
 	 */
 	public int reverseBits(int n) {
-		if (n == 0) {
-			return 0;
-		}
 		int result = 0;
 		for (int i = 0; i < 32; i++) {
-			result <<= 1;
-			result += n & 1;
+			result = (result << 1) + (n & 1);
 			n >>= 1;
 		}
 		return result;
