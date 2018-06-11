e1256e07e07513e0504681fe52b78072cbc37104
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-06-11 01:04:47 -0700

    init

--- LeetCode_Java/src/RangeAddition.java
+++ LeetCode_Java/src/RangeAddition.java
@@ -1,82 +1,86 @@
+import java.util.Arrays;
+
 /**
- * Given an m * n matrix M initialized with all 0's and several update operations.
- * Operations are represented by a 2D array, and each operation is represented by an array with two 
- * positive integers a and b, which means M[i][j] should be added by one for all 0 <= i < a and 
- * 0 <= j < b.
- * You need to count and return the number of maximum integers in the matrix after performing all 
- * the operations.
- * Example 1:
- * Input: 
- * m = 3, n = 3
- * operations = [[2,2],[3,3]]
- * Output: 4
- * Explanation: 
- * Initially, M = 
- * [[0, 0, 0],
- *  [0, 0, 0],
- *  [0, 0, 0]]
- * After performing [2,2], M = 
- * [[1, 1, 0],
- *  [1, 1, 0],
- *  [0, 0, 0]]
- *  After performing [3,3], M = 
- * [[2, 2, 1],
- *  [2, 2, 1],
- *  [1, 1, 1]]
- * So the maximum integer in M is 2, and there are four of it in M. So return 4.
- * Note:
- * 1. The range of m and n is [1,40000].
- * 2. The range of a is [1,m], and the range of b is [1,n].
- * 3. The range of operations size won't exceed 10,000.
+ * Assume you have an array of length n initialized with all 0's and are given k update operations.
+ * Each operation is represented as a triplet: [startIndex, endIndex, inc] which increments each 
+ * element of subarray A[startIndex ... endIndex] (startIndex and endIndex inclusive) with inc.
+ * Return the modified array after all k operations were executed.
+ * Example:
+ * Given:
+ *     length = 5,
+ *     updates = [
+ *         [1,  3,  2],
+ *         [2,  4,  3],
+ *         [0,  2, -2]
+ *     ]
+ * Output:
+ *     [-2, 0, 3, 5, 3]
+ * Explanation:
+ * Initial state:
+ * [ 0, 0, 0, 0, 0 ]
+ * After applying operation [1, 3, 2]:
+ * [ 0, 2, 2, 2, 0 ]
+ * After applying operation [2, 4, 3]:
+ * [ 0, 2, 5, 5, 3 ]
+ * After applying operation [0, 2, -2]:
+ * [-2, 0, 3, 5, 3 ]
  * @author wendi
  *
  */
 public class RangeAddition {
 	
+	
 	/**
-	 * Method2: Single pass
-	 * @param int m, int n, int[][] ops
-	 * @return int
-	 * Time: O(k)
+	 * Range caching
+	 * @param int length, int[] updates
+	 * @return int[]
+	 * Time: O(k+n) n=updates.length, n=length
 	 * Space: O(1)
 	 */
-	public int rangeAdditionI(int m, int n, int[][] ops) {
-		if (ops == null || ops.length == 0 || ops[0].length == 0) return m * n;	
-		for (int[] op: ops) {
-			m = Math.min(op[0], m);
-			n = Math.min(op[1], n);
-		}
-		return m * n;
+	public int[] rangeAdditionI(int length, int[][] updates) {
+        if (length <= 0) return new int[0];
+        int[] res = new int[length];
+        for (int[] op: updates) {
+            res[op[0]] += op[2];
+            if (op[1] + 1 < length) res[op[1] + 1] -= op[2];
+        }
+        int sum = 0;
+        for (int i = 0; i < res.length; i++) {
+            sum += res[i];
+            res[i] = sum;
+        }
+        return res;
 	}
 	
 	
+	
+	
+	
+	
 	/**
-	 * Method1: brute force(TLE)
-	 * @param int m, int n, int[][] ops
-	 * @return int
-	 * Time: O(m*n*k)
-	 * Space: O(m*n)
+	 * Brute force
+	 * @param int length, int[] updates
+	 * @return int[]
+	 * Time: O(kn) n=updates.length, n=length
+	 * Space: O(1)
 	 */
-	public int rangeAddition(int m, int n, int[][] ops) {
-		if (ops == null || ops.length == 0 || ops[0].length == 0) return m * n;
-		int result = 0;
-		int[][] matrix = new int[m][n];
-		for (int[] o: ops) {
-			for (int i = 0; i < o[0]; i++) {
-				for (int j = 0; j < o[1]; j++) {
-					matrix[i][j]++;
-					if (matrix[i][j] == ops.length) result++;
-				}
-			}
-		}
-		return result;
+	public int[] rangeAddition(int length, int[][] updates) {
+		if (length <= 0) return new int[0];
+        int[] res = new int[length];
+        for (int[] op: updates) {
+            for (int i = op[0]; i <= op[1]; i++) {
+                res[i] += op[2];
+            }
+        }
+        return res;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RangeAddition result = new RangeAddition();
-		System.out.println(result.rangeAddition(3, 3, new int[][] {{2,2},{3,3}}));
-		System.out.println(result.rangeAdditionI(3, 3, new int[][] {{2,2},{3,3}}));
+		int[][] updates = {{1, 3, 2}, {2, 4, 3},{0, 2, -2}};
+		System.out.println(Arrays.toString(result.rangeAddition(5, updates)));
+		System.out.println(Arrays.toString(result.rangeAdditionI(5, updates)));
 	}
 
 }
