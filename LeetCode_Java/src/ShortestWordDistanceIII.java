106f10811b187f465518a5673f7ebe9ea31a122a
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-06-13 15:11:14 -0700

    update

--- LeetCode_Java/src/ShortestWordDistanceIII.java
+++ LeetCode_Java/src/ShortestWordDistanceIII.java
@@ -33,22 +33,12 @@ public class ShortestWordDistanceIII {
 		int result = Integer.MAX_VALUE;
 		for (int i = 0; i < words.length; i++) {
 			if (words[i].equals(word1) && word1.equals(word2)) {
-				if (index1 < index2) {
-					index1 = i;
-				}
-				else {
-					index2 = i;
-				}
+				if (index1 < index2) index1 = i;
+				else index2 = i;
 			}
-			else if (words[i].equals(word1)) {
-				index1 = i;
-			}
-			else if (words[i].equals(word2)) {
-				index2 = i;
-			}
-			if (index1 != -1 && index2 != -1) {
-				result = Math.min(Math.abs(index1 - index2), result);
-			}
+			else if (words[i].equals(word1)) index1 = i;
+			else if (words[i].equals(word2)) index2 = i;
+			if (index1 != -1 && index2 != -1) result = Math.min(Math.abs(index1 - index2), result);
 		}
 		return result;
 	}
@@ -56,8 +46,8 @@ public class ShortestWordDistanceIII {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ShortestWordDistanceIII result = new ShortestWordDistanceIII();
-		System.out.println(result.shortestWordDistanceIII(new String[] {"practice", "makes", "perfect", "coding", "makes", "makes"}, 
-				"makes", "makes"));
+		System.out.println(result.shortestWordDistanceIII(
+				new String[] {"practice", "makes", "perfect", "coding", "makes", "makes"}, "makes", "makes"));
 	}
 
 }
