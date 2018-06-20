1d6bc6e1311489408313e9121da9937ef178b2e8
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-06-20 00:21:12 -0700

    update

--- LeetCode_Java/src/NumberofIslands.java
+++ LeetCode_Java/src/NumberofIslands.java
@@ -20,9 +20,68 @@
  *
  */
 public class NumberofIslands {
+	
+	
+	/**
+	 * Method2: UnionFindSet. 
+	 * @param char[][] grid
+	 * @return int
+	 * Time: O(n^2)
+	 * Space: O(n)
+	 */
+	public int numberofIslandsI(char[][] grid) {
+		if(grid == null || grid.length == 0 || grid[0].length == 0) {
+			return 0;
+		}
+		int m = grid.length;
+		int n = grid[0].length;
+		int res = 0;
+		UnionFindSet ufs = new UnionFindSet(m * n);
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j] == '0') continue;
+				res++;
+				int[] dx = {-1, 0, 1, 0};
+				int[] dy = {0, 1, 0, -1};
+				for (int k = 0; k < 4; k++) {
+					int row = i + dx[k];
+					int col = j + dy[k];
+					if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == '0') continue;
+					if (ufs.union(i * n + j, row * n + col)) res--;
+				}
+			}
+		}
+		return res;
+	}
+	
+	class UnionFindSet {
+		int[] parents;
+		public UnionFindSet(int n) {
+			parents = new int[n];
+			for (int i = 0; i < n; i++) parents[i] = i;
+		}
+		
+		public int find(int x) {
+			while (parents[x] != x) {
+				x = parents[x];
+			}
+			return x;
+		}
+		
+		public boolean union(int a, int b) {
+			int pA = find(a);
+			int pB = find(b);
+			if (pA != pB) {
+				parents[pA] = pB;
+				return true;
+			}
+			return false;
+		}
+	}
 
 	/**
-	 * DFS(Recursion). Find '1' in the grid first. Then use helper to find the union island.
+	 * Method1: DFS(Recursion). 
+	 * Find '1' in the grid first. Then use helper to find the union island.
 	 * If find, set grid[i][j]='0', means this area has already been found, it's union island,
 	 * should not count to res.
 	 * @param char[][] grid
@@ -62,6 +121,12 @@ public class NumberofIslands {
 		// TODO Auto-generated method stub
 		NumberofIslands result = new NumberofIslands();
 		System.out.println(result.numberofIslands(new char[][] 
+				{{'1', '1', '1', '1', '0'}, 
+				 {'1', '1', '0', '1', '0'},
+				 {'1', '1', '0', '0', '0'},
+				 {'1', '1', '0', '0', '0'},
+				 {'0', '0', '0', '0', '0'}}));
+		System.out.println(result.numberofIslandsI(new char[][] 
 				{{'1', '1', '1', '1', '0'}, 
 				 {'1', '1', '0', '1', '0'},
 				 {'1', '1', '0', '0', '0'},
