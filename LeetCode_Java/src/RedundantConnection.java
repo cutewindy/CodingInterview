304a2a5cd5705463d74cae2c3cda6cf1982ded09
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-06-20 16:22:04 -0700

    init

---
+++ LeetCode_Java/src/RedundantConnection.java
@@ -0,0 +1,96 @@
+import java.util.Arrays;
+
+/**
+ * In this problem, a tree is an undirected graph that is connected and has no cycles.
+ * The given input is a graph that started as a tree with N nodes (with distinct values 1, 2, ..., N), 
+ * with one additional edge added. The added edge has two different vertices chosen from 1 to N, and 
+ * was not an edge that already existed.
+ * The resulting graph is given as a 2D-array of edges. Each element of edges is a pair [u, v] with 
+ * u < v, that represents an undirected edge connecting nodes u and v.
+ * Return an edge that can be removed so that the resulting graph is a tree of N nodes. If there are 
+ * multiple answers, return the answer that occurs last in the given 2D-array. The answer edge [u, v] 
+ * should be in the same format, with u < v.
+ * Example 1:
+ * Input: [[1,2], [1,3], [2,3]]
+ * Output: [2,3]
+ * Explanation: The given undirected graph will be like this:
+ *   1
+ *  / \
+ * 2 - 3
+ * Example 2:
+ * Input: [[1,2], [2,3], [3,4], [1,4], [1,5]]
+ * Output: [1,4]
+ * Explanation: The given undirected graph will be like this:
+ * 5 - 1 - 2
+ *     |   |
+ *     4 - 3
+ * Note:
+ * The size of the input 2D-array will be between 3 and 1000.
+ * Every integer represented in the 2D-array will be between 1 and N, where N is the size of the 
+ * input array.
+ * @author wendi
+ *
+ */
+public class RedundantConnection {
+	
+	
+	/**
+	 * UnionFindSet
+	 * @param int[][] edges
+	 * @return int[]
+	 * Time: O(e)
+	 * Space: O(v)
+	 */
+	public int[] redundantConnection(int[][] edges) {
+        int[] res = new int[2];
+        int n = edges.length;
+        UnionFindSet ufs = new UnionFindSet(n + 1);
+        for (int[] e: edges) {
+            if (!ufs.union(e[0], e[1])) {
+                res = e;
+                break;
+            }
+        }
+        return res;
+    }
+    
+    class UnionFindSet {
+        int[] parents;
+        
+        public UnionFindSet(int n) {
+            parents = new int[n];
+            for (int i = 0; i < n; i++) parents[i] = i;
+        }
+        
+        public int find(int x) {
+            int input = x;
+            while (parents[x] != x) {
+                x = parents[x];
+            }
+            while (parents[input] != x) {
+                int next = parents[input];
+                parents[input] = x;
+                input = next;
+            }
+            return x;
+        }
+        
+        public boolean union(int a, int b) {
+            int pA = find(a);
+            int pB = find(b);
+            if (pA != pB) {
+                parents[pA] = pB;
+                return true;
+            }
+            return false;
+        }		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RedundantConnection result = new RedundantConnection();
+		System.out.println(Arrays.toString(result.redundantConnection(
+				new int[][] {{1,2}, {2,3}, {3,4}, {1,4}, {1,5}})));
+	}
+
+}
