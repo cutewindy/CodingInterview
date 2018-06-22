93974fcb2d958d7be5a0e716a539c28504f3d164
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-06-22 16:17:53 -0700

    update

--- LeetCode_Java/src/TrappingRainWaterII.java
+++ LeetCode_Java/src/TrappingRainWaterII.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -47,9 +48,8 @@ public class TrappingRainWaterII {
 	 * Space: O(m * n + k)
 	 */
 	public int trappingRainWaterII(int[][] heightMap) {
-		if (heightMap == null || heightMap.length == 0 || heightMap[0].length == 0) {
-			return 0;
-		}
+		if (heightMap == null || heightMap.length == 0 || heightMap[0].length == 0) return 0;
+		
 		int m = heightMap.length;
 		int n = heightMap[0].length;
 		int result = 0;
@@ -60,6 +60,7 @@ public class TrappingRainWaterII {
 			}
 		});
 		boolean[][] visited = new boolean[m][n];
+		
 		// init, add all the Nodes which are on borders to the queue.
 		for (int j = 0; j < n; j++) {   
 			heap.add(new Node(0, j, heightMap[0][j])); // top
@@ -67,12 +68,13 @@ public class TrappingRainWaterII {
 			heap.add(new Node(m - 1, j, heightMap[m - 1][j])); // bottom
 			visited[m - 1][j] = true;
 		}
-		for (int i = 0; i < m; i++) {  
+		for (int i = 1; i < m - 1; i++) {  
 			heap.add(new Node(i, 0, heightMap[i][0]));  // left
 			visited[i][0] = true;
 			heap.add(new Node(i, n - 1, heightMap[i][n - 1]));  // right
 			visited[i][n - 1] = true;
 		}
+		
 		// update heap and result
 		int[][] dire = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 		while (!heap.isEmpty()) {
@@ -80,9 +82,7 @@ public class TrappingRainWaterII {
 			for (int i = 0; i < dire.length; i++) {
 				int row = curr.x + dire[i][0];
 				int col = curr.y + dire[i][1];
-				if (row < 0 || row >= m || col < 0 || col >= n || visited[row][col]) {
-					continue;   // check whether out of bound or is visited
-				}
+				if (row < 0 || row >= m || col < 0 || col >= n || visited[row][col]) continue;   // check whether out of bound or is visited				
 				if (heightMap[row][col] < curr.h) {
 					result += curr.h - heightMap[row][col];
 					heightMap[row][col] = curr.h;
@@ -97,6 +97,7 @@ public class TrappingRainWaterII {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TrappingRainWaterII result = new TrappingRainWaterII();
+		System.out.println(result.trappingRainWaterII(new int[][] {{6, 4, 4, 3, 6},{5, 1, 2, 2, 1},{6, 5, 1, 5, 6},{6, 6, 5, 6, 6}}));
 		System.out.println(result.trappingRainWaterII(new int[][] {{1,4,3,1,3,2} ,{3,2,1,3,2,4} ,{2,3,3,2,3,1}}));
 	}
 
