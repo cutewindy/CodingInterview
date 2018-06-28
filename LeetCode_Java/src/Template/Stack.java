2fc1af3ca48a1113b22a89303ee04f0b36800fb5
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-06-28 11:14:09 -0700

    init

---
+++ LeetCode_Java/src/Template/Stack.java
@@ -0,0 +1,31 @@
+package Template;
+/**
+ * 
+ * Similar Questions:
+ * 150. Evaluate Reverse Polish Notation
+ * 224. Basic Calculator
+ * 227. Basic Calculator II
+ * 241. Different Ways to Add Parentheses
+ * 282. Expression Add Operators
+ * 341. Flatten Nested List Iterator
+ * 385. Mini Parser
+ * 394. Decode String
+ * 439. Ternary Expression Parser
+ * 471. Encode String with Shortest Length
+ * 722. Remove Comments
+ * 726. Number of Atoms
+ * 736. Parse Lisp Expression
+ * 770. Basic Calculator IV
+ * 772. Basic Calculator III
+ * 
+ * @author wendi
+ *
+ */
+public class Stack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
