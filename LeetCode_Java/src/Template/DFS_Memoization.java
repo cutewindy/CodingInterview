72321d81a9556130c9ec386570385896b60020f0
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-06-29 22:49:02 -0700

    init

---
+++ LeetCode_Java/src/Template/DFS_Memoization.java
@@ -0,0 +1,15 @@
+package Template;
+/**
+ * 494 Target Sum
+ * 514 Freedom Trail	
+ * @author wendi
+ *
+ */
+public class DFS_Memoization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
