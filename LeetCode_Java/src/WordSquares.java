fcee8d25ad802e378563339271a8daf06d3da982
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-07-09 22:32:48 -0700

    init

--- LeetCode_Java/src/WordSquares.java
+++ LeetCode_Java/src/WordSquares.java
@@ -59,23 +59,100 @@ import java.util.List;
  */
 public class WordSquares {
 	
+	class TrieNode{
+		List<String> startWith;
+		TrieNode[] children;
+		public TrieNode() {
+			this.startWith = new ArrayList<>();
+			this.children = new TrieNode[26];
+		}		
+	}
+	
+	class Trie{
+		private TrieNode root;
+		
+		public Trie(String[] words) {
+			root = new TrieNode();
+			for (String word: words) {
+				TrieNode curr = root;
+				for (char c: word.toCharArray()) {
+					if (curr.children[c - 'a'] == null) curr.children[c - 'a'] = new TrieNode();
+					curr.children[c - 'a'].startWith.add(word);
+					curr = curr.children[c - 'a'];
+				}
+			}
+		}
+		
+		private List<String> findPrefix(String word) {
+			TrieNode curr = root;
+			for (char c: word.toCharArray()) {
+				if (curr.children[c - 'a'] == null) return new ArrayList<String>();
+				curr = curr.children[c - 'a'];
+			}
+			return curr.startWith;
+		}
+	}
+	
 	/**
-	 * 
+	 * Trie + Backtracking  
+	 * "https://leetcode.com/problems/word-squares/discuss/91333/Explained.-My-Java-solution-using-Trie-126ms-1616"
+	 * check the validity of the word square while we build it.
+	 * Example: ["area","lead","wall","lady","ball"]
+	 * We know that the sequence contains 4 words because the length of each word is 4.
+	 * Every word can be the first word of the sequence, let's take "wall" for example.
+	 * Which word could be the second word? Must be a word start with "a" (therefore "area"), 
+	 * because it has to match the second letter of word "wall".
+	 * Which word could be the third word? Must be a word start with "le" (therefore "lead"), 
+	 * because it has to match the third letter of word "wall" and the third letter of word "area".
+	 * What about the last word? Must be a word start with "lad" (therefore "lady"). For the same 
+	 * reason above.
+	 * In order for this to work, we need to fast retrieve all the words with a given prefix. There 
+	 * could be 2 ways doing this:
+	 * 1. Using a hashtable, key is prefix, value is a list of words with that prefix.
+	 * 2. Trie, we store a list of words with the prefix on each trie node.
+	 * The implemented below uses Trie.
 	 * @param String[] words
 	 * @return List<List<String>>
+	 * Time: O()
+	 * Space: O()
+	 * Stack space: O()
 	 */
 	public List<List<String>> wordSquares(String[] words) {
-		List<List<String>> result = new ArrayList<>();
-		if (words == null || words.length == 0) {
-			return result;
+		List<List<String>> res = new ArrayList<>();
+		if (words == null || words.length == 0) return res;
+		Trie trie = new Trie(words);
+		List<String> path = new ArrayList<>();
+		for (String word: words) {
+			path.add(word);
+			dfs(word.length(), trie, path, res);
+			path.remove(path.size() - 1);
 		}
-		return result;
+		return res;
 	}
+	
+	private void dfs(int len, Trie trie, List<String> path, List<List<String>> res) {
+		if (path.size() == len) {
+			res.add(new ArrayList<>(path));
+			return;
+		}
+		int i = path.size();
+		StringBuilder prefix = new StringBuilder();
+		for (String word: path) {
+			prefix.append(word.charAt(i));
+		}
+		List<String> startWith = trie.findPrefix(prefix.toString());
+		for (String sw: startWith) {
+			path.add(sw);
+			dfs(len, trie, path, res);
+			path.remove(path.size() - 1);
+		}
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WordSquares result = new WordSquares();
-		System.out.println(result.wordSquares(new String[] {""}));
+		System.out.println(result.wordSquares(new String[] {"area","lead","wall","lady","ball"}));
 	}
 
 }
