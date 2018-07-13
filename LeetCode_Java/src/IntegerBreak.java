70ec0f8f56c546f1bf5209fbe8007f4c98b7d914
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-07-12 17:43:10 -0700

    update

--- LeetCode_Java/src/IntegerBreak.java
+++ LeetCode_Java/src/IntegerBreak.java
@@ -44,11 +44,10 @@ public class IntegerBreak {
 	 * Space: O(n)
 	 */
 	public int integerBreak(int n) {
-		if (n < 3) return 1;
+		if (n == 0) return 0;
 		int[] dp = new int[n + 1];
 		dp[1] = 1;
-		dp[2] = 1;
-		for (int i = 3; i <= n; i++) {
+		for (int i = 2; i <= n; i++) {
 			for (int j = 1; j < i; j++) {
 				dp[i] = Math.max(Math.max(j, dp[j]) * Math.max(i - j, dp[i - j]), dp[i]);
 			}
