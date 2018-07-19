94fb70327749698e4c82ea00bce56c86968c9848
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-07-19 11:55:56 -0700

    update

--- LeetCode_Java/src/FindPeakElement.java
+++ LeetCode_Java/src/FindPeakElement.java
@@ -30,15 +30,9 @@ public class FindPeakElement {
 		int end = nums.length - 1;
 		while (start + 1 < end) {
 			int mid = start + (end - start) / 2;
-			if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
-				return mid;
-			}
-			else if (nums[mid - 1] < nums[mid] && nums[mid] < nums[mid + 1]) {
-				start = mid;
-			}
-			else {
-				end = mid;
-			}
+			if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) return mid;
+			else if (nums[mid - 1] < nums[mid] && nums[mid] < nums[mid + 1]) start = mid;
+			else end = mid;
 		}
 		return nums[start] > nums[end] ? start : end;
 	}
