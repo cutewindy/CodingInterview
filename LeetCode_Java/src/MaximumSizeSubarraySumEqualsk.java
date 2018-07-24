2fb2765422325c9b8b54dffea1cd0a6ff53dfbf2
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-07-24 14:01:48 -0700

    update

--- LeetCode_Java/src/MaximumSizeSubarraySumEqualsk.java
+++ LeetCode_Java/src/MaximumSizeSubarraySumEqualsk.java
@@ -30,18 +30,14 @@ public class MaximumSizeSubarraySumEqualsk {
 	 * Space: On)
 	 */
 	public int maximumSizeSubarraySumEqualsk(int[] nums, int k) {
-		if (nums == null || nums.length == 0) {
-			return 0;
-		}
+		if (nums == null || nums.length == 0) return 0;
 		int result = 0;
 		Map<Integer, Integer> hash = new HashMap<>();
+		hash.put(0, -1);
 		int sum = 0;
 		for (int i = 0; i < nums.length; i++) {     
 			sum += nums[i];
-			if (sum == k) {                         // subarray [0, i]
-				result = Math.max(i + 1, result);   
-			}
-			else if (hash.containsKey(sum - k)) {   // subarray (hash.get(sum-k), i]
+			if (hash.containsKey(sum - k)) {   // subarray (hash.get(sum-k), i]
 				result = Math.max(i - hash.get(sum - k), result);
 			}
 			if (!hash.containsKey(sum)) {   // keep only 1st duplicate as we want longest size
