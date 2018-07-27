8f57cd83dde385d9e47709f0596a9758d48b6d4a
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-07-26 17:16:09 -0700

    update

--- LeetCode_Java/src/MergeSortedArray.java
+++ LeetCode_Java/src/MergeSortedArray.java
@@ -23,19 +23,13 @@ public class MergeSortedArray {
 		if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) return;
 		int p1 = m - 1;
 		int p2 = n - 1;
-		int i = m + n - 1;
 		while (p1 >= 0 && p2 >= 0) {
-			if (nums1[p1] > nums2[p2]) {
-				nums1[i--] = nums1[p1--];
-			}
-			else {
-				nums1[i--] = nums2[p2--];
-			}
+			if (nums1[p1] > nums2[p2]) nums1[p1 + p2 + 1] = nums1[p1--];
+			else nums1[p1 + p2 + 1] = nums2[p2--];
 		}
 		while (p2 >= 0) {
-			nums1[i--] = nums2[p2--];
+			nums1[p2] = nums2[p2--];
 		}
-		return;
 	}
 	
 	public static void main(String[] args) {
