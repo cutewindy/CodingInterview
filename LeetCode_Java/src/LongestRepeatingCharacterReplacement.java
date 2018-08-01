113bbb39ba30ddb554f5bf79d53548793cdc97ac
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-08-01 10:59:39 -0700

    init

---
+++ LeetCode_Java/src/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,44 @@
+/**
+ * Given a string that consists of only uppercase English letters, you can replace any letter in the 
+ * string with another letter at most k times. Find the length of a longest substring containing all 
+ * repeating letters you can get after performing the above operations.
+ * Note:
+ * Both the string's length and k will not exceed 104.
+ * Example 1:
+ * Input:
+ * s = "ABAB", k = 2
+ * Output:
+ * 4
+ * Explanation:
+ * Replace the two 'A's with two 'B's or vice versa.
+ * Example 2:
+ * Input:
+ * s = "AABABBA", k = 1
+ * Output:
+ * 4
+ * Explanation:
+ * Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+ * The substring "BBBB" has the longest repeating letters, which is 4.
+ * @author wendi
+ *
+ */
+public class LongestRepeatingCharacterReplacement {
+	
+	/**
+	 * 
+	 * @param String s, int k
+	 * @return int
+	 * Time: O()
+	 * Space: O()
+	 */
+	public int longestRepeatingCharacterReplacement(String s, int k) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LongestRepeatingCharacterReplacement result = new LongestRepeatingCharacterReplacement();
+		System.out.println(result.longestRepeatingCharacterReplacement("AABABBA", 1));
+	}
+
+}
