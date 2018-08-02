3c0788c093851cb18be197881691d28eaf26ad28
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-08-02 14:02:15 -0700

    update

--- LeetCode_Java/src/MinimumSizeSubarraySum.java
+++ LeetCode_Java/src/MinimumSizeSubarraySum.java
@@ -16,6 +16,28 @@
 public class MinimumSizeSubarraySum {
 	
 	/**
+	 * sliding window other template
+	 * @param int s, int[] nums
+	 * @return int
+	 * Time: O(n)
+	 * Space: O(1)
+	 */
+	public int minimumSizeSubarraySumI(int s, int[] nums) {
+		if (nums == null || nums.length == 0) return 0;
+		int res = Integer.MAX_VALUE;
+		int sum = 0;
+		for (int start = 0, end = 0; start < nums.length; start++) {
+			while (end < nums.length && sum < s) {
+				sum += nums[end];
+				end++;
+			}
+			if (sum >= s && res > end - start) res = end - start;
+			sum -= nums[start];
+		}
+		return res == Integer.MAX_VALUE ? 0 : res;
+	}
+	
+	/**
 	 * Two pointers(head, i): slide window: while(sum>=s) minimum size = min(i - head + 1), 
 	 * move the head of slide window by head+1, else move the tail of slide window by i + 1.
 	 * @param int s, int[] nums
@@ -46,6 +68,8 @@ public class MinimumSizeSubarraySum {
 		MinimumSizeSubarraySum result = new MinimumSizeSubarraySum();
 		System.out.println(result.minimumSizeSubarraySum(7, new int[] {2, 3, 1, 2, 4, 3}));
 		System.out.println(result.minimumSizeSubarraySum(3, new int[] {1, 1}));
+		System.out.println(result.minimumSizeSubarraySumI(7, new int[] {2, 3, 1, 2, 4, 3}));
+		System.out.println(result.minimumSizeSubarraySumI(3, new int[] {1, 1}));
 	}
 
 }
