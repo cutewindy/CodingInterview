43d138a81f36aea50d4081c02ac4e8e8304ba373
cutewindy <wendi@Wendi-Air.attlocal.net>
2018-08-06 23:50:08 -0700

    init

---
+++ LeetCode_Java/src/Template/DP.java
@@ -0,0 +1,29 @@
+package Template;
+/**
+ * DP Problem:
+ * 1. maximum/ minimum
+ * 2. yes/ no
+ * 3. count
+ * 
+ * 动态规划4点要素：
+ * 1. State 状态: 灵感，创造力，存储小规模问题的结果
+ * 2. Function 方程： 状态之间的联系，怎么通过小的状态，来算大的状态
+ * 3. Initialization 初始化：最极限的小状态是什么，起点
+ * 4. Answer 答案： 最大的那个状态是什么，终点
+ * 
+ * 最常见的四种类型：
+ * 1. Matrix DP
+ * 2. Sequence
+ * 3. Two Sequence DP
+ * 4. Others
+ * @author wendi
+ *
+ */
+public class DP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
