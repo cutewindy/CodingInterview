c7e030164b486e817218489f3c0b3ef22ab2abc6
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-08-10 17:37:46 -0700

    update

--- LeetCode_Java/src/ShuffleanArray.java
+++ LeetCode_Java/src/ShuffleanArray.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 /**
  * // Init an array with set 1, 2, and 3.
@@ -16,49 +17,89 @@ import java.util.List;
  * 
  * Tags: 
  * @author wendi
- *
+ * 
+// FY Shuffle和Reservoir Sampling的比较：
+// +--------------+----------------+-----------------------------+------+-------+
+// | ALGORITHM    | INPUT(S)       | OUTPUT                      | TIME | SPACE |
+// +--------------+----------------+-----------------------------+------+-------+
+// | FISHER-YATES | n elements     | A list containing all n     | O(n) | O(n)  |
+// | SHUFFLE      |                | elements in a random order  |      |       |
+// +--------------+----------------+-----------------------------+------+-------+
+// | RESERVOIR    | n elements and | A set containing k of the n | O(n) | O(k)  |
+// | SAMPLING     | an integer k   | elements, selected randomly |      |       |
+// +--------------+----------------+-----------------------------+------+-------+
  */
 public class ShuffleanArray {
 	
-	/**
-	 * Using an int[] array to save the original nums
-	 * reset(): just return array
-	 * shuffle(): using a list to save the array, each time choose a num randomly from list, then 
-	 * 	 		  remove it from list, until size = 0;
-	 */
-	int[] array;
+	int[] nums;
 	public ShuffleanArray(int[] nums) {
-		array = nums;
+		this.nums = nums;
 	}
 	
 	/** Resets the array to its original configuration and return it. */
 	public int[] reset() {
-		return array;
+		return nums;
 	}
 	
 	/** Returns a random shuffling of the array. */
-	public int[] shuffle() {
-		int[] result = new int[array.length];
-		List<Integer> list = new ArrayList<>();
-		for (int i = 0; i < array.length; i++) {
-			list.add(array[i]);
+	/**
+	 * Method2: array 
+	 * using a array to keep the res, each time choose a index j from [0, i] randomly, swap (i, j), 
+	 * the probability should be (n-1)/n * (n-2)/(n-1) * ... * i/(i+1) * 1/i = 1/n. Beside the last 
+	 * one, they are the probability that j haven't been choose, and the last one is the probability 
+	 * that j have been choose. Thus, the total probability of j can be choose is 1/n.
+	 * Time: O(n)
+	 * Space: O(1)
+	 */	
+	public int[] shuffleI() {
+		int n = nums.length;
+		int[] res = nums.clone();
+		Random r = new Random();
+		for (int i = n - 1; i > 0; i--) {
+			int j = r.nextInt(i + 1);
+			swap(res, i, j);
 		}
-		for (int i = 0; i < array.length; i++) {
+		return res;
+	}
+	
+	private void swap(int[] nums, int i, int j) {
+		if (i == j) return;
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+	}
+	
+	/** Returns a random shuffling of the array. */
+	/**
+	 * Method1: List 
+	 * using a array to save the array, each time choose a num randomly from list, then remove it 
+	 * from list, until size = 0;
+	 * Time: O(n)
+	 * Space: O(n)
+	 */
+	public int[] shuffle() {
+		int[] result = new int[nums.length];
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < nums.length; i++) list.add(nums[i]);
+		for (int i = 0; i < nums.length; i++) {
 			int index = (int) (Math.random() * list.size());
-//			System.out.println(index);
 			result[i] = list.get(index);
 			list.remove(index);
 		}
 		return result;
 	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ShuffleanArray result = new ShuffleanArray(new int[] {1, 2, 3, 4, 5});
-		System.out.println(Arrays.toString(result.reset()));
-		System.out.println(Arrays.toString(result.shuffle()));
-		System.out.println(Arrays.toString(result.shuffle()));
-		System.out.println(Arrays.toString(result.reset()));
+		System.out.println("reset:   " + Arrays.toString(result.reset()));
+		System.out.println("shuffle: " + Arrays.toString(result.shuffle()));
+		System.out.println("shuffle: " + Arrays.toString(result.shuffle()));
+		System.out.println("reset:   " + Arrays.toString(result.reset()));
+		System.out.println("shuffle: " + Arrays.toString(result.shuffleI()));
+		System.out.println("shuffle: " + Arrays.toString(result.shuffleI()));
+		System.out.println("reset:   " + Arrays.toString(result.reset()));
 	}
 
 }
