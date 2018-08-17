949aea6f42e8e32ffd54bff78c8d03e1718ebba9
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-08-17 12:22:08 -0700

    update

--- LeetCode_Java/src/CountofSmallerNumbersAfterSelf.java
+++ LeetCode_Java/src/CountofSmallerNumbersAfterSelf.java
@@ -30,11 +30,54 @@ public class CountofSmallerNumbersAfterSelf {
 		}
 	}
 	
+    class SegmentTree {
+        public SegmentTreeNode root;
+        
+        public SegmentTree (int start, int end) {
+            root = build(start, end);
+        }
+        
+        public SegmentTreeNode build(int start, int end) {
+            SegmentTreeNode root = new SegmentTreeNode(start, end);
+            if (start == end) return root;
+            int mid = start + (end - start) / 2;
+            root.left = build(start, mid);
+            root.right = build(mid + 1, end);
+            return root;
+        }
+        
+        public int query(int val) {
+            return query(root, val);
+        }
+        
+        public int query(SegmentTreeNode root, int val) {
+            if (root.end <= val) return root.count;
+            int mid = root.start + (root.end - root.start) / 2;
+            if (mid >= val) return query(root.left, val);
+            return query(root.left, val) + query(root.right, val);
+        }
+        
+        public void modify(int val) {
+            modify(root, val);
+        }
+        
+        public void modify(SegmentTreeNode root, int val) {
+            if (root.start == val && root.end == val) {
+                root.count++;
+                return;
+            }
+            int mid = root.start + (root.end - root.start) / 2;
+            if (mid >= val) modify(root.left, val);
+            else modify(root.right, val);
+            root.count = root.left.count + root.right.count;
+        }
+    }
+	
 	/**
 	 * SegmentTree
 	 * @param int[] nums
 	 * @return List<Integer>
-	 * Time: O(n + n + 2log(n))
+	 * Time: O(n + n + n*2log(n))
 	 * Space: O(n)
 	 */
 	public List<Integer> countofSmallerNumbersAfterSelf(int[] nums) {
@@ -46,50 +89,14 @@ public class CountofSmallerNumbersAfterSelf {
         	min = Math.min(num, min);
         	max = Math.max(num, max);
         }
-        SegmentTreeNode root = build(min, max);
+        SegmentTree ST = new SegmentTree(min, max);
         for (int i = nums.length - 1; i >= 0; i--) {
-        	if (nums[i] == min) res.add(0, 0);
-        	else res.add(0, query(root, nums[i] - 1));
-        	modify(root, nums[i]);
+        	if (nums[i] == min) res.add(0, 0);  // take care when nums[i]==min
+        	else res.add(0, ST.query(nums[i] - 1));
+        	ST.modify(nums[i]);
         }
         return res;
     }
-	
-	/**
-	 * O(n)
-	 */
-	private SegmentTreeNode build(int start, int end) {
-		SegmentTreeNode root = new SegmentTreeNode(start, end);
-		if (start == end) return root;
-		int mid = start + (end - start) / 2;
-		root.left = build(start, mid);
-		root.right = build(mid + 1, end);
-		return root;
-	}
-	
-	/**
-	 * O(log(n))
-	 */
-	private int query(SegmentTreeNode root, int index) {
-		if (root.end <= index) return root.count;
-		int mid = root.start + (root.end - root.start) / 2;
-		if (index <= mid) return query(root.left, index);
-		return query(root.left, index) + query(root.right, index);
-	}
-	
-	/**
-	 * O(log(n))
-	 */
-	private void modify(SegmentTreeNode root, int index) {
-		if (root.start == index && root.end == index) {
-			root.count++;
-			return;
-		}
-		int mid = root.start + (root.end - root.start) / 2;
-		if (index <= mid) modify(root.left, index);
-		else modify(root.right, index);
-		root.count = root.left.count + root.right.count;
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
