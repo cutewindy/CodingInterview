d14900877a0467e8d2be2a6309b5a0261be585f5
cutewindy <wendi@Wendi-Air.attlocal.net>
2018-08-22 16:33:44 -0700

    init

--- LeetCode_Java/src/wePay/Alerter.java
+++ LeetCode_Java/src/wePay/Alerter.java
@@ -1,8 +1,30 @@
 package wePay;
-
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
+/**
+ * The Alerter is a simple monitoring tool, intended to help detect increases in response time for 
+ * some process. It does that by computing a few statistics about the process across a 'window' of 
+ * a certain number of runs, and alerting (returning true) if certain thresholds are met.
+ * 
+ * It takes the following parameters:
+ * - inputs: A list of integer times for the process. This list may by very long
+ * - window size: how many runs long a window is, as an integer
+ * - allowedIncrease: how far over 'average' a window or value is allowed to by, as a percent. This 
+ * is represented as a decimal value based on one, so a 50% allowable increase would be represented 
+ * as 1.5
+ * 
+ * Your Alerter should return true if either of the following conditions are met:
+ * * Any value is more than the allowed increase above the window average in All windows in which it 
+ * appears.
+ * For example:
+ * alert({1,2,100,2,2}, 3, 1.5) should alert: the value 100 appears in three window, and in all cases
+ * is more than 50% over the average value
+ * alert({1,2,4,2,2}, 3, 2) should not alert: the outlier is 4, and that value appears in a window
+ * with average value 2.6, less than 100% of that average
+ * * Any window's average is more than the acceptable increase over a previous window's average value
+ * For example:
+ * alert({1,2,100,2,2}, 2, 2.5) should alert: Even though no individual value causes an alert, there 
+ * is a window with average 1.5 and a later window with an average more than 2.5 times larger
+ * Otherwise, you should return false.
+ */
 
 public class Alerter {
 	
