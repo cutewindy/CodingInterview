91ea92e5a8bea4bd49461c53c7a826d8a868b213
cutewindy <wendi@Wendi-Air.attlocal.net>
2018-08-25 18:20:15 -0700

    init

---
+++ LeetCode_Java/src/CourseScheduleIII.java
@@ -0,0 +1,78 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * There are n different online courses numbered from 1 to n. Each course has some duration(course 
+ * length) t and closed on dth day. A course should be taken continuously for t days and must be 
+ * finished before or on the dth day. You will start at the 1st day.
+ * Given n online courses represented by pairs (t,d), your task is to find the maximal number of 
+ * courses that can be taken.
+ * Example:
+ * Input: [[100, 200], [200, 1300], [1000, 1250], [2000, 3200]]
+ * Output: 3
+ * Explanation: 
+ * There're totally 4 courses, but you can take 3 courses at most:
+ * First, take the 1st course, it costs 100 days so you will finish it on the 100th day, and ready 
+ * to take the next course on the 101st day.
+ * Second, take the 3rd course, it costs 1000 days so you will finish it on the 1100th day, and 
+ * ready to take the next course on the 1101st day. 
+ * Third, take the 2nd course, it costs 200 days so you will finish it on the 1300th day. 
+ * The 4th course cannot be taken now, since you will finish it on the 3300th day, which exceeds the 
+ * closed date.
+ * Note:
+ * 1. The integer 1 <= d, t, n <= 10,000.
+ * 2. You can't take two courses simultaneously.
+ * @author wendi
+ *
+ */
+public class CourseScheduleIII {
+
+	/**
+	 * Greedy: Arraysort + MaxHeap
+	 * First: Sort the courses by their deadlines (Greedy! We have to deal with courses with early 
+	 * deadlines first)
+	 * Second: add current course to a maxHeap. If time exceeds, drop the previous course which 
+	 * costs the most time. (That must be the best choice!)
+	 * @param int[][] courses
+	 * @return int
+	 * Time: O(nlog(n)) n = courses.length
+	 * Space: O(n)
+	 */
+	public int courseScheduleIII(int[][] courses) {
+		if (courses == null || courses.length == 0) return 0;
+		Arrays.sort(courses, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] a, int[] b) {
+				return a[1] - b[1];
+			}
+		});
+		PriorityQueue<int[]> maxHeap = new PriorityQueue<>(10, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] a, int[] b) {
+				return b[0] - a[0]; 
+			}
+		});
+		int res = 0;
+		int time = 0;
+		for (int[] course: courses) {
+			res++;
+			time += course[0];
+			maxHeap.offer(course);
+			if (time > course[1]) {
+				time -= maxHeap.poll()[0];
+				res--;
+			}
+		}
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CourseScheduleIII result = new CourseScheduleIII();
+		System.out.println(result.courseScheduleIII(new int[][] {{100, 200}, {200, 1300}, {1000, 1250}, {2000, 3200}}));
+		System.out.println(result.courseScheduleIII(new int[][] {{5, 5}, {2, 6}, {4, 6}}));
+		System.out.println(result.courseScheduleIII(new int[][] {{100, 2}, {32, 50}}));
+	}
+
+}
