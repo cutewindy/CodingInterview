a1b552e62e9a2ffec18954091403b8305cf363af
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-09-11 11:51:54 -0700

    update

--- LeetCode_Java/src/CountandSay.java
+++ LeetCode_Java/src/CountandSay.java
@@ -12,34 +12,25 @@
 public class CountandSay {
 	
 	/**
-	 * two pointer, i used for index which c, count used for how many time c occur.
+	 * sliding window, two pointer
 	 * @param n
 	 * @return String 
 	 * Time: O(n * 2^n)  2^n:the longest length of subresult
 	 * Space: O(2^n)
 	 */
     public String countandSay(int n) {
-        if (n == 0) {
-            return "";
-        }
-        String result = "1";
-        for (int i = 1; i < n; i++) {
-            String curr = result;
-            result = "";
-            int count = 1;
-            for (int j = 0; j < curr.length(); j++) {
-                if (j + 1 < curr.length() && curr.charAt(j) == curr.charAt(j + 1)) {
-                    count++;
-                }
-                else {
-                    result += String.valueOf(count) + String.valueOf(curr.charAt(j));
-                    count = 1;
-                }
+        String res = "1";
+        while (--n > 0) {
+            StringBuilder sb = new StringBuilder();
+            char[] resArray = res.toCharArray();
+            for (int start = 0, end = 0; start < resArray.length; start = end) {
+                while (end < resArray.length && resArray[end] == resArray[start]) end++;
+                sb.append(end - start).append(resArray[start]);
             }
+            res = sb.toString();
         }
-        return result;
+        return res;
     }
-    
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
