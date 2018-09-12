3ed95f8e251bb367f1607f21da8ddf5fa12b233a
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-09-11 22:56:32 -0700

    update

--- LeetCode_Java/src/google_OA/EmailProcess.java
+++ LeetCode_Java/src/google_OA/EmailProcess.java
@@ -16,7 +16,7 @@ import java.util.Map;
  * 'ab+work@example.com' -> 'ab@example.com'。
  * 处理完后的邮件地址一样的放在一组，返回所有组，里面不止一个邮件地址的组的个数。
  * 
- * 请问楼主对于第一题...ab@example, ...ab..@example, ...ab..+cd@example应该是什么结果呢？ 前两个不变， ...
+ * 请问楼主对于第一题...ab@example, ...ab..@example, ...ab..+cd..@example应该是什么结果呢？ 前两个不变， ...
  * 不确定，我处理的是前两个不变hh，我觉得都可以吧
  * @author wendi
  *
@@ -48,11 +48,23 @@ public class EmailProcess {
 	
 	private String process(String email) {
 		String[] parts = email.split("@");
-		parts[0] = parts[0].replaceAll("\\.", "");
-		for (int i = 0; i < parts[0].length(); i++) {
-			if (parts[0].charAt(i) == '+') return parts[0].substring(0, i) + parts[1];
+		StringBuilder sb = new StringBuilder();
+		int n = parts[0].length();
+		int start = 0, end = n - 1;
+		System.out.println("s: " + start + " e: " + end);
+		while (start < n && parts[0].charAt(start) == '.') start++;
+		while (end >= 0 && parts[0].charAt(end) == '.') end--;
+		System.out.println("s: " + start + " e: " + end);
+		for (int i = start; i <= end; i++) {
+			if (parts[0].charAt(i) == '.') continue;
+			if (parts[0].charAt(i) == '+') {
+				System.out.println(parts[0].substring(0, start) + sb.toString() + "@" + parts[1]);
+				return parts[0].substring(0, start) + sb.toString() + "@" + parts[1];
+			}
+			sb.append(parts[0].charAt(i));
 		}
-		return parts[0] + parts[1];
+		System.out.println(parts[0].substring(0, start) + sb.toString() + parts[0].substring(end + 1, n) + "@" + parts[1]);
+		return parts[0].substring(0, start) + sb.toString() + parts[0].substring(end + 1, n) + "@" + parts[1];
 	}
 
 	public static void main(String[] args) {
@@ -61,7 +73,7 @@ public class EmailProcess {
 		System.out.println(result.emailProcess(new ArrayList<>(Arrays.asList(
 				"a.b@example.com", "dupli......cate@example.com", "d.u.p.l.i.c.a.t.e@example.com", "ab+work@example.com"))));
 		System.out.println(result.emailProcess(new ArrayList<>(Arrays.asList(
-				"...ab@example", "...ab..@example","...ab..+cd@example"))));
+				"...ab@example", "...ab..@example","...ab..+cd..@example"))));
 	}
 
 }
