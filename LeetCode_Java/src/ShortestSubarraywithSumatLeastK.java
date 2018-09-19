184401105ac0c6800fa4ea876bf26643f35a5a0e
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-09-18 22:54:01 -0700

    init

---
+++ LeetCode_Java/src/ShortestSubarraywithSumatLeastK.java
@@ -0,0 +1,39 @@
+/**
+ * Return the length of the shortest, non-empty, contiguous subarray of A with sum at least K.
+ * If there is no non-empty subarray with sum at least K, return -1.
+ * Example 1:
+ * Input: A = [1], K = 1
+ * Output: 1
+ * Example 2:
+ * Input: A = [1,2], K = 4
+ * Output: -1
+ * Example 3:
+ * Input: A = [2,-1,2], K = 3
+ * Output: 3
+ * Note:
+ * 1. 1 <= A.length <= 50000
+ * 2. -10 ^ 5 <= A[i] <= 10 ^ 5
+ * 3. 1 <= K <= 10 ^ 9
+ * @author wendi
+ *
+ */
+public class ShortestSubarraywithSumatLeastK {
+	
+	/**
+	 * 
+	 * @param int[] A, int K
+	 * @return int
+	 * Time: O()
+	 * Space: O()
+	 */
+	public int ShortestSubarraywithSumatLeastK(int[] A, int K) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ShortestSubarraywithSumatLeastK result = new ShortestSubarraywithSumatLeastK();
+		System.out.println(result.ShortestSubarraywithSumatLeastK(new int[] {-28,81,-20,28,-29}, 89));
+	}	
+
+}
