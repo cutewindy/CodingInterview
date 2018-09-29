af0d9262bee1c01a8da0df58e78e4b4d5720b730
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-09-28 21:02:59 -0700

    update

--- LeetCode_Java/src/CheapestFlightsWithinKStops.java
+++ LeetCode_Java/src/CheapestFlightsWithinKStops.java
@@ -1,8 +1,8 @@
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
@@ -41,42 +41,107 @@ import java.util.Queue;
  */
 public class CheapestFlightsWithinKStops {
 	
-	
 	/**
-	 * 
+	 * Approach4: PriorityQueue
 	 * @param int n, int[][] flights, int src, int dst, int K
 	 * @return int
-	 * Time: O()
-	 * Space: O()
+	 * Time: O(n^2)
+	 * Space: O(n)
+	 */
+	public int cheapestFlightsWithinKStopsIII(int n, int[][] flights, int src, int dst, int K) {
+        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+        buildGraph(flights, graph);
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(new Comparator<int[]>() {    // [dst, stop, price]
+        	@Override
+        	public int compare(int[] a, int[] b) {
+        		return a[2] - b[2]; 
+        	}
+        });
+        minHeap.offer(new int[] {src, K, 0});
+        while (!minHeap.isEmpty()) {
+        	int[] curr = minHeap.poll();
+        	int u = curr[0];
+        	int stop = curr[1];
+        	int price = curr[2];
+        	if (u == dst) return price;
+        	if (stop < 0 || !graph.containsKey(u)) continue;
+        	for (Integer v: graph.get(u).keySet()) {
+        		minHeap.offer(new int[] {v, stop - 1, price + graph.get(u).get(v)});
+        	}
+        }
+        return -1;
+	}
+	
+	
+	/**
+	 * Approach2: DFS
+	 * @param int n, int[][] flights, int src, int dst, int K
+	 * @return int
+	 * Time: O(n^2)
+	 * Space: O(n)
+	 * Stack space: O(K)
+	 */
+	public int cheapestFlightsWithinKStopsI(int n, int[][] flights, int src, int dst, int K) {
+        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+        buildGraph(flights, graph);
+        int[] res = new int[] {Integer.MAX_VALUE};
+        dfs(src, dst, K + 1, 0, graph, res);
+        return res[0] == Integer.MAX_VALUE ? -1 : res[0];
+	}
+	
+	private void dfs(int src, int dst, int K, int price, Map<Integer, Map<Integer, Integer>> graph, int[] res) {
+		if (K < 0) return;
+		if (src == dst) {
+			res[0] = Math.min(price, res[0]);
+			return;
+		}
+		if (price >= res[0] || !graph.containsKey(src)) return; // important, prunning
+		for (Integer next: graph.get(src).keySet()) {
+			dfs(next, dst, K - 1, price + graph.get(src).get(next), graph, res);
+		}
+	}
+	
+	
+	/**
+	 * Approach1: BFS
+	 * @param int n, int[][] flights, int src, int dst, int K
+	 * @return int
+	 * Time: O(n^2)
+	 * Space: O(n)
 	 */
 	public int cheapestFlightsWithinKStops(int n, int[][] flights, int src, int dst, int K) {
-        if (src == dst) return 0;
-        Integer[] costs = new Integer[n];
-        costs[src] = 0;
-        Map<Integer, List<int[]>> map = new HashMap<>();
-        for (int[] f: flights) {
-            if (!map.containsKey(f[0])) map.put(f[0], new ArrayList<int[]>());
-            map.get(f[0]).add(f);
-        }
-        Queue<Integer> queue = new LinkedList<>();
-        if (!map.containsKey(src)) return -1;
-        queue.offer(src);
+        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+        buildGraph(flights, graph);
+        Queue<int[]> queue = new LinkedList<>();
+        int res = Integer.MAX_VALUE;
+        queue.offer(new int[] {src, 0});   // [node, price]
         while (!queue.isEmpty() && K-- >= 0) {
             int size = queue.size();
-            Integer[] temp = costs.clone();
             while (size-- > 0) {
-                int u = queue.poll();
-                if (!map.containsKey(u)) continue;
-                for (int[] v: map.get(u)) {
-                    if (temp[v[1]] == null || temp[v[1]] > costs[u] + v[2]) {
-                        temp[v[1]] = costs[u] + v[2];
-                        queue.offer(v[1]);
+                int[] curr = queue.poll();
+                int u = curr[0];
+                int price = curr[1];
+                if (!graph.containsKey(u)) continue;
+                for (Integer v: graph.get(u).keySet()) {
+                    int newPrice = graph.get(u).get(v) + price;
+                    if (newPrice >= res) continue;   // important, prunning
+                    if (v == dst) {
+                        res = Math.min(newPrice, res);
+                    }
+                    else {
+                        queue.offer(new int[] {v, newPrice});
                     }
                 }
             }
-            costs = temp;
         }
-        return costs[dst] == null ? -1 : costs[dst];        
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+    
+    private void buildGraph(int[][] flights, Map<Integer, Map<Integer, Integer>> graph) {
+        for (int[] f: flights) {
+            if (!graph.containsKey(f[0])) graph.put(f[0], new HashMap<Integer, Integer>());
+            graph.get(f[0]).put(f[1], f[2]);
+        }        
     }	
 	
 
@@ -84,6 +149,10 @@ public class CheapestFlightsWithinKStops {
 		// TODO Auto-generated method stub
 		CheapestFlightsWithinKStops result = new CheapestFlightsWithinKStops();
 		System.out.println(result.cheapestFlightsWithinKStops
+				(3, new int[][] {{0,1,100},{1,2,100},{0,2,500}}, 0, 2, 1));
+		System.out.println(result.cheapestFlightsWithinKStopsI
+				(3, new int[][] {{0,1,100},{1,2,100},{0,2,500}}, 0, 2, 1));
+		System.out.println(result.cheapestFlightsWithinKStopsIII
 				(3, new int[][] {{0,1,100},{1,2,100},{0,2,500}}, 0, 2, 1));
 	}
 
