728d8f0e3a125b53a42c19464c6bcf38b04f92d6
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-10-11 09:49:36 -0700

    update

--- LeetCode_Java/src/PathSumIII.java
+++ LeetCode_Java/src/PathSumIII.java
@@ -26,6 +26,38 @@ import java.util.Map;
  *
  */
 public class PathSumIII {
+	
+	/**
+	 * Method3: backtracking + prefixSum + hashMap
+	 * So the idea is similar as Two sum, using HashMap to store (key: the prefix sum, value: how 
+	 * many ways get to this prefix sum) , and whenever reach a node, we check if prefix sum - target 
+	 * exists in hashmap or not, if it does, we added up the ways of prefix sum - target into res.
+	 * For instance : in one path we have 1,2,-1,-1,2, then the prefix sum will be: 1, 3, 2, 1, 3, 
+	 * let's say we want to find target sum is 2, then we will have{2}, {1,2,-1}, {2,-1,-1,2} and 
+	 * {2} ways.
+	 * @param TreeNode root, int sum
+	 * @return int
+	 * Time:O(n)
+	 * Space: O(nlog(n))
+	 */
+	public int pathSumIIIII(TreeNode root, int sum) {
+		if (root == null) return 0;
+		int[] res = new int[1];
+		Map<Integer, Integer> map = new HashMap<>();// [key, value] = [sum, freq]
+		map.put(0, 1);
+		dfs(root, sum, 0, map, res); 
+		return res[0];
+	}
+	
+	private void dfs(TreeNode root, int target, int currSum, Map<Integer, Integer> map, int[] res) {
+		if (root == null) return;
+		currSum += root.val;
+		if (map.containsKey(currSum - target)) res[0] += map.get(currSum - target);
+		map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+		dfs(root.left, target, currSum, map, res);
+		dfs(root.right, target, currSum, map, res);
+        map.put(currSum, map.get(currSum) - 1);  // take care, don't forget
+	}
 
 	
 	/**
@@ -51,26 +83,30 @@ public class PathSumIII {
 	
 	
 	/**
-	 * Method1: DFS(Bottom up)
+	 * Method1: DFS(top down) + hashmap
 	 * @param TreeNode root, int sum
 	 * @return int
-	 * Time:O(n^2)
+	 * Time:O(nlog(n))
 	 * Space: O(nlog(n))
 	 */
 	public int pathSumIII(TreeNode root, int sum) {
 		if (root == null) return 0;
-		return helper(root, sum, new HashMap<Integer, Integer>());
+		int[] res = new int[1];
+		dfs(root, sum, new HashMap<Integer, Integer>(), res);
+		return res[0];
 	}
 	
-	public int helper(TreeNode root, int sum, Map<Integer, Integer> hash) {
-		if (root == null) return 0;
-		int result = 0;
+	public void dfs(TreeNode root, int sum, Map<Integer, Integer> hash, int[] res) {
+		if (root == null) return;
 		Map<Integer, Integer> newHash = new HashMap<>();
 		// update hash
-		for (Integer key: hash.keySet()) newHash.put(key + root.val, hash.get(key));
-		newHash.put(root.val, newHash.getOrDefault(root.val, 0) + 1);
-		result += newHash.getOrDefault(sum, 0);
-		return result + helper(root.left, sum, newHash) + helper(root.right, sum, newHash);
+		newHash.put(root.val, 1);
+		for (Integer key: hash.keySet()) {
+			newHash.put(root.val + key, newHash.getOrDefault(root.val + key, 0) + hash.get(key));
+		}
+		res[0] += newHash.getOrDefault(sum, 0);
+		dfs(root.left, sum, newHash, res); 
+		dfs(root.right, sum, newHash, res);
 	}
 	
 	public static void main(String[] args) {
@@ -80,6 +116,8 @@ public class PathSumIII {
 		TreeNode root = TreeNode.generateCBT(new int[] {0, 1, 1});
 		System.out.println(result.pathSumIII(root, 1));
 		System.out.println(result.pathSumIIII(root, 1));
+		System.out.println(result.pathSumIIIII(root, 1));
 	}
+	
 
 }
