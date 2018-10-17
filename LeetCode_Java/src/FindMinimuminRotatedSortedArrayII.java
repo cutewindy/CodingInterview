f1bc2dec5979198faa43e4e4ed09bd24d8819570
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-10-17 09:35:30 -0700

    update

--- LeetCode_Java/src/FindMinimuminRotatedSortedArrayII.java
+++ LeetCode_Java/src/FindMinimuminRotatedSortedArrayII.java
@@ -31,6 +31,9 @@ public class FindMinimuminRotatedSortedArrayII {
 		int end = nums.length - 1;
 		while (start + 1 < end) {
 			int mid = start + (end - start) / 2;
+			if (nums[mid] < nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+				return nums[mid];
+			}
 			if (nums[mid] == nums[end]) {
 				end--;    // take care for the case {3, 3, 1, 3}
 			}
