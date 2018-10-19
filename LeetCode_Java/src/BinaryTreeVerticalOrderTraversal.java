8bca4caa037f4588265ef443f328e4fd6fe44815
cutewindy <wendi@Wendi-MacBook-Air.local>
2018-10-18 21:06:28 -0700

    update

--- LeetCode_Java/src/BinaryTreeVerticalOrderTraversal.java
+++ LeetCode_Java/src/BinaryTreeVerticalOrderTraversal.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 /**
@@ -65,9 +67,57 @@ import java.util.Queue;
  */
 public class BinaryTreeVerticalOrderTraversal {
 	
+	/**
+	 * Approach2: BFS + HashMap
+	 * 1. BFS, put node, col into queue at the same time
+	 * 2. Every left child access col - 1 while right child col + 1
+	 * 3. This maps node into different col buckets
+	 * 4. Get col boundary min and max on the fly
+	 * 5. Retrieve result from cols
+	 * @param TreeNode root
+	 * @return List<List<Integer>>
+	 * Time: O(n)
+	 * Space: O(n)
+	 */
+	public List<List<Integer>> binaryTreeVerticalOrderTraversalI(TreeNode root) {
+		List<List<Integer>> res = new ArrayList<>();
+		if (root == null) return res;
+		
+		//use a HashMap to store the TreeNode and the according cloumn value
+		Map<Integer, List<Integer>> map = new HashMap<>(); // [key, value]: [column, list]
+		int min = Integer.MAX_VALUE;
+		Queue<TreeNode> qNode = new LinkedList<>();
+		Queue<Integer> qIdx = new LinkedList<>();
+		qNode.offer(root);
+		qIdx.offer(0);
+		while (!qNode.isEmpty()) {
+			TreeNode currNode = qNode.poll();
+			int currIdx = qIdx.poll();
+			if (!map.containsKey(currIdx)) map.put(currIdx, new ArrayList<Integer>());
+			map.get(currIdx).add(currNode.val);
+			if (currNode.left != null) {
+				qNode.offer(currNode.left);
+				qIdx.offer(currIdx - 1);
+			}
+			if (currNode.right != null) {
+				qNode.offer(currNode.right);
+				qIdx.offer(currIdx + 1);
+			}
+			min = Math.min(currIdx, min);
+		}
+		
+		//update min ,min means the minimum column value, which is the left most node
+		while (map.containsKey(min)) {
+			res.add(map.get(min));
+			min++;
+		}
+		
+		return res;
+	}
+	
 	
 	/**
-	 * BFS
+	 * Approach1: BFS
 	 * 1. calculate the rang first, then insert value into corresponding buckets.
 	 * 2. put node, index into queue at the same time. Every left child access index - 1 while right 
 	 *    child index + 1
@@ -125,6 +175,7 @@ public class BinaryTreeVerticalOrderTraversal {
 		TreeNode root = TreeNode.generateCBT(new int[] {3,9,8,4,0,1,7,2,3,5});
 		TreeNode.printCBT(root);
 		System.out.println(result.binaryTreeVerticalOrderTraversal(root));
+		System.out.println(result.binaryTreeVerticalOrderTraversalI(root));
 	}
 
 }
