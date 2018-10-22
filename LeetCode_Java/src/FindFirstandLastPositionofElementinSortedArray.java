4190e2119b660a9ae7a65fe4b5d45ff439eb5e90
cutewindy <wendi@Wendi-Air.attlocal.net>
2018-10-21 23:26:15 -0700

    update

--- LeetCode_Java/src/FindFirstandLastPositionofElementinSortedArray.java
+++ LeetCode_Java/src/FindFirstandLastPositionofElementinSortedArray.java
@@ -17,13 +17,13 @@ import java.util.Arrays;
 public class FindFirstandLastPositionofElementinSortedArray {
 	
 	/**
-	 * Binary search
+	 * Approach2: Binary search, one pass
 	 * @param int[] nums, int target
 	 * @return int[]
 	 * Time: O(log(n))
 	 * Space:O(1)
 	 */
-	public int[] findFirstandLastPositionofElementinSortedArray(int[] nums, int target) {
+	public int[] findFirstandLastPositionofElementinSortedArrayI(int[] nums, int target) {
 		if (nums == null || nums.length == 0) return new int[] {-1, -1};
 		int start = 0;
 		int end = nums.length - 1;
@@ -42,12 +42,58 @@ public class FindFirstandLastPositionofElementinSortedArray {
 		else if (nums[end] == target) return new int[] {end, end};
 		return new int[] {-1, -1};
 	}
+	
+	
+	
+	/**
+	 * Approach1: Binary search, two pass
+	 * @param int[] nums, int target
+	 * @return int[]
+	 * Time: O(log(n))
+	 * Space:O(1)
+	 */
+	public int[] findFirstandLastPositionofElementinSortedArray(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return new int[] {-1, -1};
+        int start = findStart(nums, target);
+        if (start == -1) return new int[] {-1, -1};
+        int end = findEnd(nums, target);
+        return new int[] {start, end};
+    }
+    
+    private int findStart(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target) start = mid;
+            else end = mid;
+        }
+        if (nums[start] == target) return start;
+        if (nums[end] == target) return end;
+        return -1;
+    }
+    
+    private int findEnd(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) start = mid;
+            else end = mid;
+        }
+        if (nums[end] == target) return end;
+        if (nums[start] == target) return start;
+        return -1;
+	}
+    
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FindFirstandLastPositionofElementinSortedArray result = new FindFirstandLastPositionofElementinSortedArray();
 		System.out.println(Arrays.toString(result.findFirstandLastPositionofElementinSortedArray(new int[] {5,7,7,8,8,10}, 8)));
 		System.out.println(Arrays.toString(result.findFirstandLastPositionofElementinSortedArray(new int[] {5,7,7,8,8,10}, 6)));
+		System.out.println(Arrays.toString(result.findFirstandLastPositionofElementinSortedArrayI(new int[] {5,7,7,8,8,10}, 8)));
+		System.out.println(Arrays.toString(result.findFirstandLastPositionofElementinSortedArrayI(new int[] {5,7,7,8,8,10}, 6)));
 	}
 
 }
