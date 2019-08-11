c3098f1844e573c7f3781684d1dab8d18bdde719
cutewindy <wendi@Wendi-MacBook-Air.local>
2019-08-10 17:11:20 -0700

    update

--- LeetCode_Java/src/TwoSumIVInputisaBST.java
+++ LeetCode_Java/src/TwoSumIVInputisaBST.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -27,8 +29,71 @@ import java.util.Set;
  */
 public class TwoSumIVInputisaBST {
 	
+	
 	/**
-	 * Using HashSet
+	 * Approach3: use binary search method. For each node, we check if k - node.val exists in this 
+	 * BST.
+	 * @param TreeNode root, int k
+	 * @return boolean
+	 * Time: O(nlog(n))
+	 * Space: O(1)
+	 * Stack space: O(log(n))
+	 */
+	public boolean twoSumIVInputisaBSTII(TreeNode root, int k) {	
+        return dfs(root, root, k);
+    }
+    
+    private boolean dfs(TreeNode root, TreeNode curr, int k) {
+        if (curr == null) return false;
+        if (find(root, curr, k - curr.val)) return true;
+        return dfs(root, curr.left, k) || dfs(root, curr.right, k);
+    }
+    
+    private boolean find(TreeNode root, TreeNode p1, int target) {
+        if (root == null) return false;
+        if (root != p1 && root.val == target) return true;
+        if (root.val < target) return find(root.right, p1, target);
+        return find(root.left, p1, target);
+	}
+	
+	
+	/**
+	 * Approach2: use a sorted array to save the values of the nodes in the BST by using an inorder
+	 *  traversal. Then, we use two pointers which begins from the start and end of the array to 
+	 *  find if there is a sum k
+	 * @param TreeNode root, int k
+	 * @return boolean
+	 * Time: O(n)
+	 * Space: O(n)
+	 */
+	public boolean twoSumIVInputisaBSTI(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        inorderTraverse(root, list);
+        int s = 0;
+        int e = list.size() - 1;
+        while (s < e) {
+            int sum = list.get(s) + list.get(e);
+            if (sum == k) return true;
+            if (sum < k) s++;
+            else e--;
+        }
+        return false;
+    }
+    
+    private void inorderTraverse(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        inorderTraverse(root.left, list);
+        list.add(root.val);
+        inorderTraverse(root.right, list);
+    }
+    
+    
+    
+	/**
+	 * Approach1: Using HashSet
+	 * This method also works for those who are not BSTs. The idea is to use a set to save the 
+	 * values of the nodes in the BST. Each time when we insert the value of a new node into the 
+	 * set, we check if the set contains k - node.val.
 	 * @param TreeNode root, int k
 	 * @return boolean
 	 * Time: O(n)
